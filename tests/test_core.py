2e5099a15dc5bb15c5763baf688dfab3d0f954ac
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-07-31 13:50:48 +0100

    More core tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,15 +1,48 @@
 
+import sys
 import unittest
+import unittest.mock as mock
 
-from opsdroid.core import OpsDroid
+sys.modules['sys'].exit = mock.MagicMock()
+
+from opsdroid.core import OpsDroid  # noqa: E402
+from opsdroid.message import Message  # noqa: E402
 
 
 class TestCore(unittest.TestCase):
     """Test the opsdroid core class."""
 
-    def setup(self):
-        return OpsDroid()
-
     def test_core(self):
-        opsdroid = self.setup()
-        self.assertIsInstance(opsdroid, OpsDroid)
+        with OpsDroid() as opsdroid:
+            self.assertIsInstance(opsdroid, OpsDroid)
+
+    def test_exit(self):
+        with OpsDroid() as opsdroid:
+            sys.modules['sys'].exit.mock_calls = []
+            opsdroid.exit()
+            self.assertEqual(len(sys.modules['sys'].exit.mock_calls), 1)
+
+    def test_critical(self):
+        with OpsDroid() as opsdroid:
+            sys.modules['sys'].exit.mock_calls = []
+            opsdroid.critical("An error", 1)
+            self.assertEqual(len(sys.modules['sys'].exit.mock_calls), 1)
+
+    def test_load_regex_skill(self):
+        with OpsDroid() as opsdroid:
+            regex = r".*"
+            skill = mock.MagicMock()
+            opsdroid.load_regex_skill(regex, skill)
+            self.assertEqual(len(opsdroid.skills), 1)
+            self.assertEqual(opsdroid.skills[0]["regex"], regex)
+            self.assertIsInstance(opsdroid.skills[0]["skill"], mock.MagicMock)
+
+    def test_parse(self):
+        with OpsDroid() as opsdroid:
+            regex = r".*"
+            skill = mock.MagicMock()
+            mock_connector = mock.MagicMock()
+            opsdroid.load_regex_skill(regex, skill)
+            message = Message("Hello world", "user", "default", mock_connector)
+            opsdroid.parse(message)
+            self.assertEqual(len(skill.mock_calls), 1)
