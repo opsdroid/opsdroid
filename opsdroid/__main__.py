45374e8a3b61bab58b93efc0ce178484fbf75448
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2016-08-26 11:34:39 +0100

    Argparse (#41)
    * Add arg parse and --gen-config method

--- opsdroid/__main__.py
+++ opsdroid/__main__.py
@@ -1,6 +1,9 @@
 """Starts opsdroid."""
 
+import sys
+import os
 import logging
+import argparse
 
 from opsdroid.loader import Loader
 from opsdroid.core import OpsDroid
@@ -8,11 +11,30 @@ from opsdroid.helper import set_logging_level
 from opsdroid.const import LOG_FILENAME
 
 
+def parse_args(args):
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description='Run opsdroid.')
+    parser.add_argument('--gen-config', action="store_true",
+                        help='prints out an example configuration file')
+    return parser.parse_args(args)
+
+
 def main():
     """The main function."""
     logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO)
     logging.info("="*40)
     logging.info("Stated application")
+
+    args = parse_args(sys.argv[1:])
+
+    if args.gen_config:
+        path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "configuration/example_configuration.yaml")
+        with open(path, 'r') as conf:
+            print(conf.read())
+        sys.exit(0)
+
     with OpsDroid() as opsdroid:
         loader = Loader(opsdroid)
         opsdroid.config = loader.load_config_file([
