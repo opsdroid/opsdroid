29ec80f10caf6bb3c36c5e18c8d19ae220dc378d
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-12-16 15:38:08 +0000

    More api.ai tests

--- tests/test_parser_apiai.py
+++ tests/test_parser_apiai.py
@@ -36,3 +36,55 @@ class TestParserApiai(asynctest.TestCase):
                 await apiai.parse_apiai(opsdroid, message)
 
             self.assertTrue(mock_skill.called)
+
+    async def test_parse_apiai_failure(self):
+        with OpsDroid() as opsdroid:
+            opsdroid.config['parsers'] = {
+                    'apiai': {'access-token': "test"}
+                }
+            mock_skill = amock.CoroutineMock()
+            match_apiai('myaction')(mock_skill)
+
+            mock_connector = amock.CoroutineMock()
+            message = Message("Hello world", "user", "default", mock_connector)
+
+            with amock.patch.object(apiai, 'call_apiai') as mocked_call_apiai:
+                mocked_call_apiai.return_value = {
+                        "result": {
+                            "action": "myaction",
+                            "score": 0.7
+                        },
+                        "status": {
+                            "code": 404,
+                            "errorType": "not found"
+                        }
+                    }
+                await apiai.parse_apiai(opsdroid, message)
+
+            self.assertFalse(mock_skill.called)
+
+    async def test_parse_apiai_low_score(self):
+        with OpsDroid() as opsdroid:
+            opsdroid.config['parsers'] = {
+                    'apiai': {'access-token': "test", "min-score": 0.8}
+                }
+            mock_skill = amock.CoroutineMock()
+            match_apiai('myaction')(mock_skill)
+
+            mock_connector = amock.CoroutineMock()
+            message = Message("Hello world", "user", "default", mock_connector)
+
+            with amock.patch.object(apiai, 'call_apiai') as mocked_call_apiai:
+                mocked_call_apiai.return_value = {
+                        "result": {
+                            "action": "myaction",
+                            "score": 0.7
+                        },
+                        "status": {
+                            "code": 200,
+                            "errorType": "success"
+                        }
+                    }
+                await apiai.parse_apiai(opsdroid, message)
+
+            self.assertFalse(mock_skill.called)
