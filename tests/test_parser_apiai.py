06befc07ee5e22d084ebd8e0b2347e7e4136d401
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-12-16 16:04:52 +0000

    More tests

--- tests/test_parser_apiai.py
+++ tests/test_parser_apiai.py
@@ -37,6 +37,33 @@ class TestParserApiai(asynctest.TestCase):
 
             self.assertTrue(mock_skill.called)
 
+    async def test_parse_apiai_raises(self):
+        with OpsDroid() as opsdroid:
+            opsdroid.config['parsers'] = {
+                    'apiai': {'access-token': "test"}
+                }
+            mock_skill = amock.CoroutineMock()
+            mock_skill.side_effect = Exception()
+            match_apiai('myaction')(mock_skill)
+
+            mock_connector = amock.CoroutineMock()
+            message = Message("Hello world", "user", "default", mock_connector)
+
+            with amock.patch.object(apiai, 'call_apiai') as mocked_call_apiai:
+                mocked_call_apiai.return_value = {
+                        "result": {
+                            "action": "myaction",
+                            "score": 0.7
+                        },
+                        "status": {
+                            "code": 200,
+                            "errorType": "success"
+                        }
+                    }
+                await apiai.parse_apiai(opsdroid, message)
+
+            self.assertTrue(mock_skill.called)
+
     async def test_parse_apiai_failure(self):
         with OpsDroid() as opsdroid:
             opsdroid.config['parsers'] = {
--- tests/test_parser_regex.py
+++ tests/test_parser_regex.py
@@ -23,26 +23,17 @@ class TestParserRegex(asynctest.TestCase):
 
             self.assertTrue(mock_skill.called)
 
-    # async def test_parse_regex_raises(self):
-    #     with OpsDroid() as opsdroid:
-    #         mock_skill = amock.CoroutineMock()
-    #         mock_skill.side_effect = Exception()
-    #         match_regex(r"(.*)")(mock_skill)
-    #         self.assertEqual(len(opsdroid.skills), 1)
-    #
-    #         mock_connector = amock.CoroutineMock()
-    #         message = Message("Hello world", "user",
-    #                           "default", mock_connector)
-    #
-    #         with self.assertRaises(Exception):
-    #             await parse_regex(opsdroid, message)
+    async def test_parse_regex_raises(self):
+        with OpsDroid() as opsdroid:
+            mock_skill = amock.CoroutineMock()
+            mock_skill.side_effect = Exception()
+            match_regex(r"(.*)")(mock_skill)
+            self.assertEqual(len(opsdroid.skills), 1)
 
-    async def test_raises(self):
-        mock_obj = amock.CoroutineMock()
-        mock_obj.side_effect = Exception()
+            mock_connector = amock.CoroutineMock()
+            message = Message("Hello world", "user",
+                              "default", mock_connector)
 
-        async def wrapper(wrapped_object):
-            await wrapped_object()
+            await parse_regex(opsdroid, message)
 
-        with self.assertRaises(Exception):
-            await wrapper(mock_obj())
+            self.assertTrue(mock_skill.called)
