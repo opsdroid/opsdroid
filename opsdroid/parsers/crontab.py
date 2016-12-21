1993bb616086be33711194c0f7ce4f49d2fa5e50
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-12-21 13:47:19 +0000

    More tests

--- opsdroid/parsers/crontab.py
+++ opsdroid/parsers/crontab.py
@@ -7,18 +7,13 @@ from datetime import datetime
 import pycron
 
 
-def run_forever(opsdroid):
-    """Check if the opsdroid event loop is running."""
-    return opsdroid.eventloop.is_running()
-
-
 async def parse_crontab(opsdroid):
     """Parse all crontab skills against the current time."""
     # pylint: disable=broad-except
     # We want to catch all exceptions coming from a skill module and not
     # halt the application. If a skill throws an exception it just doesn't
     # give a response to the user, so an error response should be given.
-    while run_forever(opsdroid):
+    while opsdroid.eventloop.is_running():
         await asyncio.sleep(60 - datetime.now().time().second)
         logging.debug("Running crontab skills")
         for skill in opsdroid.skills:
