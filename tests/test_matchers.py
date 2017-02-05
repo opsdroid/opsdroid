68813bfb1a00ac43d2d41f3a23fd9b48d18f22af
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-02-05 09:51:00 +0000

    Test webhook mock response wrapper

--- tests/test_matchers.py
+++ tests/test_matchers.py
@@ -1,16 +1,18 @@
 
-import unittest
-import unittest.mock as mock
+import asynctest
+import asynctest.mock as mock
+
+import aiohttp.web
 
 from opsdroid.core import OpsDroid
 from opsdroid.web import Web
 from opsdroid import matchers
 
 
-class TestMatchers(unittest.TestCase):
+class TestMatchers(asynctest.TestCase):
     """Test the opsdroid matcher decorators."""
 
-    def test_match_regex(self):
+    async def test_match_regex(self):
         with OpsDroid() as opsdroid:
             regex = r"(.*)"
             mockedskill = mock.MagicMock()
@@ -20,7 +22,7 @@ class TestMatchers(unittest.TestCase):
             self.assertEqual(opsdroid.skills[0]["regex"], regex)
             self.assertIsInstance(opsdroid.skills[0]["skill"], mock.MagicMock)
 
-    def test_match_apiai(self):
+    async def test_match_apiai(self):
         with OpsDroid() as opsdroid:
             action = "myaction"
             mockedskill = mock.MagicMock()
@@ -36,7 +38,7 @@ class TestMatchers(unittest.TestCase):
             self.assertEqual(opsdroid.skills[1]["apiai_intent"], intent)
             self.assertIsInstance(opsdroid.skills[1]["skill"], mock.MagicMock)
 
-    def test_match_crontab(self):
+    async def test_match_crontab(self):
         with OpsDroid() as opsdroid:
             crontab = "* * * * *"
             mockedskill = mock.MagicMock()
@@ -46,7 +48,7 @@ class TestMatchers(unittest.TestCase):
             self.assertEqual(opsdroid.skills[0]["crontab"], crontab)
             self.assertIsInstance(opsdroid.skills[0]["skill"], mock.MagicMock)
 
-    def test_match_webhook(self):
+    async def test_match_webhook(self):
         with OpsDroid() as opsdroid:
             opsdroid.loader.current_import_config = {"name": "testhook"}
             opsdroid.web_server = Web(opsdroid)
@@ -60,3 +62,18 @@ class TestMatchers(unittest.TestCase):
             self.assertIsInstance(opsdroid.skills[0]["skill"], mock.MagicMock)
             self.assertEqual(
                 opsdroid.web_server.web_app.router.add_post.call_count, 2)
+
+    async def test_match_webhook_response(self):
+        with OpsDroid() as opsdroid:
+            opsdroid.loader.current_import_config = {"name": "testhook"}
+            opsdroid.web_server = Web(opsdroid)
+            opsdroid.web_server.web_app = mock.Mock()
+            webhook = "test"
+            mockedskill = mock.CoroutineMock()
+            decorator = matchers.match_webhook(webhook)
+            decorator(mockedskill)
+            postcalls, _ = \
+                opsdroid.web_server.web_app.router.add_post.call_args_list[0]
+            wrapperfunc = postcalls[1]
+            webhookresponse = await wrapperfunc(None)
+            self.assertEqual(type(webhookresponse), aiohttp.web.Response)
