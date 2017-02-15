1b676735bb71d6b69afb3d2501b1850036156d6d
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-02-15 16:12:04 +0000

    Create helper function for getting the existing opsdroid instance

--- opsdroid/helper.py
+++ opsdroid/helper.py
@@ -1,1 +1,10 @@
 """Helper functions to use within OpsDroid."""
+
+
+def get_opsdroid():
+    """Return the running opsdroid instance."""
+    from opsdroid.core import OpsDroid
+    if len(OpsDroid.instances) == 1:
+        return OpsDroid.instances[0]
+    else:
+        return None
--- opsdroid/matchers.py
+++ opsdroid/matchers.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from opsdroid.core import OpsDroid
+from opsdroid.helper import get_opsdroid
 from opsdroid.web import Web
 
 
@@ -13,10 +13,10 @@ def match_regex(regex):
     """Return regex match decorator."""
     def matcher(func):
         """Add decorated function to skills list for regex matching."""
-        for opsdroid in OpsDroid.instances:
-            opsdroid.skills.append({"regex": regex, "skill": func,
-                                    "config":
-                                    opsdroid.loader.current_import_config})
+        opsdroid = get_opsdroid()
+        opsdroid.skills.append({"regex": regex, "skill": func,
+                                "config":
+                                opsdroid.loader.current_import_config})
         return func
     return matcher
 
@@ -25,10 +25,10 @@ def match_apiai_action(action):
     """Return apiai action match decorator."""
     def matcher(func):
         """Add decorated function to skills list for apiai matching."""
-        for opsdroid in OpsDroid.instances:
-            opsdroid.skills.append({"apiai_action": action, "skill": func,
-                                    "config":
-                                    opsdroid.loader.current_import_config})
+        opsdroid = get_opsdroid()
+        opsdroid.skills.append({"apiai_action": action, "skill": func,
+                                "config":
+                                opsdroid.loader.current_import_config})
         return func
     return matcher
 
@@ -37,10 +37,10 @@ def match_apiai_intent(intent):
     """Return apiai intent match decorator."""
     def matcher(func):
         """Add decorated function to skills list for apiai matching."""
-        for opsdroid in OpsDroid.instances:
-            opsdroid.skills.append({"apiai_intent": intent, "skill": func,
-                                    "config":
-                                    opsdroid.loader.current_import_config})
+        opsdroid = get_opsdroid()
+        opsdroid.skills.append({"apiai_intent": intent, "skill": func,
+                                "config":
+                                opsdroid.loader.current_import_config})
         return func
     return matcher
 
@@ -49,10 +49,10 @@ def match_crontab(crontab):
     """Return crontab match decorator."""
     def matcher(func):
         """Add decorated function to skills list for crontab matching."""
-        for opsdroid in OpsDroid.instances:
-            opsdroid.skills.append({"crontab": crontab, "skill": func,
-                                    "config":
-                                    opsdroid.loader.current_import_config})
+        opsdroid = get_opsdroid()
+        opsdroid.skills.append({"crontab": crontab, "skill": func,
+                                "config":
+                                opsdroid.loader.current_import_config})
         return func
     return matcher
 
@@ -61,23 +61,23 @@ def match_webhook(webhook):
     """Return webhook match decorator."""
     def matcher(func):
         """Add decorated function to skills list for webhook matching."""
-        for opsdroid in OpsDroid.instances:
-            config = opsdroid.loader.current_import_config
-            opsdroid.skills.append({"webhook": webhook, "skill": func,
-                                    "config": config})
+        opsdroid = get_opsdroid()
+        config = opsdroid.loader.current_import_config
+        opsdroid.skills.append({"webhook": webhook, "skill": func,
+                                "config": config})
 
-            async def wrapper(req, opsdroid=opsdroid, config=config):
-                """Wrap up the aiohttp handler."""
-                _LOGGER.info("Running skill %s via webhook", webhook)
-                opsdroid.stats["webhooks_called"] = \
-                    opsdroid.stats["webhooks_called"] + 1
-                await func(opsdroid, config, req)
-                return Web.build_response(200, {"called_skill": webhook})
+        async def wrapper(req, opsdroid=opsdroid, config=config):
+            """Wrap up the aiohttp handler."""
+            _LOGGER.info("Running skill %s via webhook", webhook)
+            opsdroid.stats["webhooks_called"] = \
+                opsdroid.stats["webhooks_called"] + 1
+            await func(opsdroid, config, req)
+            return Web.build_response(200, {"called_skill": webhook})
 
-            opsdroid.web_server.web_app.router.add_post(
-                "/skill/{}/{}".format(config["name"], webhook), wrapper)
-            opsdroid.web_server.web_app.router.add_post(
-                "/skill/{}/{}/".format(config["name"], webhook), wrapper)
+        opsdroid.web_server.web_app.router.add_post(
+            "/skill/{}/{}".format(config["name"], webhook), wrapper)
+        opsdroid.web_server.web_app.router.add_post(
+            "/skill/{}/{}/".format(config["name"], webhook), wrapper)
 
         return func
     return matcher
