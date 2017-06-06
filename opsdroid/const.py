4e27f4bec033ec2bab797bf77a5e6c4fc41421fe
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-06-06 18:51:30 +0100

    Install module deps to ~/.opsdroid

--- opsdroid/const.py
+++ opsdroid/const.py
@@ -8,6 +8,7 @@ DEFAULT_GIT_URL = "https://github.com/opsdroid/"
 MODULES_DIRECTORY = "opsdroid-modules"
 DEFAULT_ROOT_PATH = os.path.join(os.path.expanduser("~"), ".opsdroid")
 DEFAULT_MODULES_PATH = os.path.join(DEFAULT_ROOT_PATH, "modules")
+DEFAULT_MODULE_DEPS_PATH = os.path.join(DEFAULT_ROOT_PATH, "site-packages")
 DEFAULT_CONFIG_PATH = os.path.join(DEFAULT_ROOT_PATH, "configuration.yaml")
 DEFAULT_MODULE_BRANCH = "master"
 EXAMPLE_CONFIG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)),
