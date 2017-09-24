5e1f467bbbe79e68c435f880812c6061f32c2535
FabioRosado <fabioglrosado@gmail.com>
2017-09-24 17:36:33 +0100

    Include a yaml file inside another

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -33,7 +33,7 @@ class TestLoader(unittest.TestCase):
         opsdroid, loader = self.setup()
         config = loader.load_config_file(["tests/configs/minimal_with_include.yaml"])
         config2 = loader.load_config_file(["tests/configs/minimal.yaml"])
-        self.assertEqual(config, config2)
+        self.assertEqual(config['skills'], config2['skills'])
 
     def test_load_config_file_with_env_vars(self):
         opsdroid, loader = self.setup()
