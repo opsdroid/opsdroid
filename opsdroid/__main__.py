94121170a8ff0af86930710a7255a7ec095f5b82
Arthur Mandel <MandelArthurC@johndeere.com>
2017-09-30 14:42:45 -0400

    Breaking up string to multiple lines

--- opsdroid/__main__.py
+++ opsdroid/__main__.py
@@ -99,8 +99,9 @@ def welcome_message(config):
                          "http://opsdroid.readthedocs.io/#configuration")
             _LOGGER.info("Whatch the Get Started Videos at: "
                          "http://bit.ly/2fnC0Fh")
-            _LOGGER.info("Install Opsdroid Desktop at: "
-                         "https://github.com/opsdroid/opsdroid-desktop/releases")
+            _LOGGER.info("Install Opsdroid Desktop at: \n" +
+                         "https://github.com/opsdroid/opsdroid-desktop/" +
+                         "releases")
             _LOGGER.info("=" * 40)
     except KeyError:
         pass
