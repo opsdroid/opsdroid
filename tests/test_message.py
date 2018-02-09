029f2dea718907d5cd9ad1b969d04f2deb2afa80
Krishna <krishnaqmar@gmail.com>
2018-02-09 16:49:24 +0530

    shortened long lines

--- tests/test_message.py
+++ tests/test_message.py
@@ -18,12 +18,19 @@ class TestMessage(asynctest.TestCase):
             'timestamp': '01/01/2000 19:23:00',
             'messageId': '101'
         }
-        message = Message("Hello world", "user", "default", mock_connector, raw_message)
+        message = Message(
+            "Hello world", 
+            "user", 
+            "default", 
+            mock_connector,
+            raw_message)
 
         self.assertEqual(message.text, "Hello world")
         self.assertEqual(message.user, "user")
         self.assertEqual(message.room, "default")
-        self.assertEqual(message.raw_message['timestamp'], '01/01/2000 19:23:00')
+        self.assertEqual(
+            message.raw_message['timestamp'], '01/01/2000 19:23:00'
+            )
         self.assertEqual(message.raw_message['messageId'], '101')
         with self.assertRaises(NotImplementedError):
             await message.respond("Goodbye world")
