ef24016cd221af15afb22724243672f90e5f9e12
FabioRosado <fabioglrosado@gmail.com>
2018-04-20 14:51:22 +0100

    Helper function will move all .yaml files to new location, compares them and removes old one

--- opsdroid/helper.py
+++ opsdroid/helper.py
@@ -4,8 +4,10 @@ import os
 import stat
 import shutil
 import logging
+import filecmp
 
-from opsdroid.const import PRE_0_12_0_CONFIG_PATH, DEFAULT_CONFIG_PATH
+from opsdroid.const import PRE_0_12_0_CONFIG_PATH, \
+    DEFAULT_CONFIG_PATH, DEFAULT_ROOT_PATH, PRE_0_12_0_ROOT_PATH
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,12 +26,25 @@ def del_rw(action, name, exc):
     os.chmod(name, stat.S_IWRITE)
     os.remove(name)
 
+# This is meant to provide backwards compatibility for versions prior to  0.12.0
+# In the future this will probably be deleted
+
 
 def move_config_to_appdir():
-    """Move config from ~/.opsdroid to system configuration directory."""
-    if os.path.isfile(PRE_0_12_0_CONFIG_PATH):
-        shutil.copyfile(PRE_0_12_0_CONFIG_PATH, DEFAULT_CONFIG_PATH)
-        _LOGGER.info(_("Configuration file copied from {} to {} "
-                       "run opsdroid --c to edit the config "
-                       "file."), PRE_0_12_0_CONFIG_PATH, DEFAULT_CONFIG_PATH)
-        os.remove(PRE_0_12_0_CONFIG_PATH)
+    """Copy any file with .yaml extension to new appdir location."""
+    yaml_files = [file for file in os.listdir(DEFAULT_ROOT_PATH)
+                  if '.yaml' in file[-5:]]
+
+    if not os.path.isdir(DEFAULT_ROOT_PATH):
+        os.mkdir(DEFAULT_ROOT_PATH)
+
+    for file in yaml_files:
+        original_file = os.path.join(PRE_0_12_0_ROOT_PATH, file)
+        copied_file = os.path.join(DEFAULT_ROOT_PATH, file)
+        shutil.copyfile(original_file, copied_file)
+        _LOGGER.info(_('File %s copied from %s to %s '
+                       'run opsdroid -e to edit the '
+                       'main config file'), file,
+                     PRE_0_12_0_ROOT_PATH, DEFAULT_ROOT_PATH)
+        if filecmp.cmp(original_file, copied_file):
+            os.remove(original_file)
