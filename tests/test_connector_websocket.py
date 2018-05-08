687bcbdd5ba5fce0ef3482340fb10a9c5aa6a5f7
Jacob Tomlinson <jacob@tom.linson.uk>
2018-05-08 20:54:23 +0100

    Add webhook tests

--- tests/test_connector_websocket.py
+++ tests/test_connector_websocket.py
@@ -2,6 +2,7 @@ import unittest
 import asyncio
 
 import asynctest
+import asynctest.mock as amock
 
 from opsdroid.connector.websocket import ConnectorWebsocket
 
@@ -21,42 +22,26 @@ class TestConnectorWebsocket(unittest.TestCase):
         connector = ConnectorWebsocket({})
         self.assertEqual("websocket", connector.name)
 
-    # def test_connect(self):
-    #     connector = ConnectorSlack({})
-    #     with self.assertRaises(NotImplementedError):
-    #         self.loop.run_until_complete(connector.connect({}))
-
-    # def test_listen(self):
-    #     connector = ConnectorSlack({})
-    #     with self.assertRaises(NotImplementedError):
-    #         self.loop.run_until_complete(connector.listen({}))
-
-    # def test_respond(self):
-    #     connector = ConnectorSlack({})
-    #     with self.assertRaises(NotImplementedError):
-    #         self.loop.run_until_complete(connector.respond({}))
-
-    # def test_react(self):
-    #     connector = ConnectorSlack({})
-    #     reacted = self.loop.run_until_complete(connector.react({}, 'emoji'))
-    #     self.assertFalse(reacted)
-
-    # def test_user_typing(self):
-    #     opsdroid = 'opsdroid'
-    #     connector = ConnectorSlack({})
-    #     user_typing = self.loop.run_until_complete(
-    #         connector.user_typing(opsdroid, trigger=True))
-    #     assert user_typing is None
-
 
 class TestConnectorAsync(asynctest.TestCase):
     """Test the async methods of the opsdroid Websocket connector class."""
 
+    async def test_connect(self):
+        """Test the connect method adds the handlers."""
+        connector = ConnectorWebsocket({})
+        opsdroid = amock.CoroutineMock()
+        opsdroid.web_server = amock.CoroutineMock()
+        opsdroid.web_server.web_app = amock.CoroutineMock()
+        opsdroid.web_server.web_app.router = amock.CoroutineMock()
+        opsdroid.web_server.web_app.router.add_get = amock.CoroutineMock()
+        opsdroid.web_server.web_app.router.add_post = amock.CoroutineMock()
+
+        await connector.connect(opsdroid)
+
+        self.assertTrue(opsdroid.web_server.web_app.router.add_get.called)
+        self.assertTrue(opsdroid.web_server.web_app.router.add_post.called)
+
     async def test_lookup_username(self):
+        """Test lookup up the username."""
         connector = ConnectorWebsocket({})
         self.assertEqual("websocket", connector.name)
-
-    # async def test_disconnect(self):
-    #     connector = ConnectorSlack({})
-    #     res = await connector.disconnect(None)
-    #     assert res is None
