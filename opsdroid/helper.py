e03b031c2bd467e38023b4b47f93258d90d7841f
sims_dev <14924222+sims34@users.noreply.github.com>
2018-05-08 12:18:23 +0200

    Add Google style docstrings to helper.py (#534)

--- opsdroid/helper.py
+++ opsdroid/helper.py
@@ -10,7 +10,12 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def get_opsdroid():
-    """Return the running opsdroid instance."""
+    """Return the running opsdroid instance.
+
+    Returns:
+        object: opsdroid instance.
+
+    """
     from opsdroid.core import OpsDroid
     if len(OpsDroid.instances) == 1:
         return OpsDroid.instances[0]
@@ -19,7 +24,17 @@ def get_opsdroid():
 
 
 def del_rw(action, name, exc):
-    """Error handler for removing read only files."""
+    """Error handler for removing read only files.
+
+    Args:
+        action: the function that raised the exception
+        name: path name passed to the function (path and file name)
+        exc: exception information return by sys.exc_info()
+
+    Raises:
+        OsError : If the file to be removed is a directory.
+
+    """
     os.chmod(name, stat.S_IWRITE)
     os.remove(name)
 
@@ -28,7 +43,22 @@ def del_rw(action, name, exc):
 
 
 def move_config_to_appdir(src, dst):
-    """Copy any .yaml extension in "src" to "dst" and remove from "src"."""
+    """Copy any .yaml extension in "src" to "dst" and remove from "src".
+
+    Args:
+        src (str): path file.
+        dst (str): destination path.
+
+    Logging:
+        info (str): File 'my_file.yaml' copied from '/path/src/
+                       to '/past/dst/' run opsdroid -e to edit
+                       the  main config file.
+
+    Examples:
+        src : source path with .yaml file '/path/src/my_file.yaml.
+        dst : destination folder to paste the .yaml files '/path/dst/.
+
+    """
     yaml_files = [file for file in os.listdir(src)
                   if '.yaml' in file[-5:]]
 
