d174b2ca73b6c001b95c25f7e1ac331d1e854e00
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2018-07-20 12:57:01 +0100

    Gah more whitespace!
    I should stop using the GitHub editor and use an IDE!

--- opsdroid/loader.py
+++ opsdroid/loader.py
@@ -470,8 +470,8 @@ class Loader:
         skill_content = python_files[0]["content"]
         extension = os.path.splitext(python_files[0]["filename"])[1]
 
-        with tempfile.NamedTemporaryFile('w', 
-                                         delete=False, 
+        with tempfile.NamedTemporaryFile('w',
+                                         delete=False,
                                          suffix=extension) as skill_file:
             skill_file.write(skill_content)
 
