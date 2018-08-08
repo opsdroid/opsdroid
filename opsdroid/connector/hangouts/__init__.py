9681c7dd7317d13badf3ac71cdeb932c1e454a31
gabeduke <gabeduke@gmail.com>
2018-08-08 00:40:24 -0400

    clean up a bit

--- opsdroid/connector/hangouts/__init__.py
+++ opsdroid/connector/hangouts/__init__.py
@@ -1,17 +1,10 @@
 """A connector for Slack."""
 import logging
-import asyncio
-import json
-import re
-import argparse
-import os
-import appdirs
 
 import aiohttp
 import hangups
 
 from opsdroid.connector import Connector
-from opsdroid.message import Message
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -30,7 +23,6 @@ class ConnectorHangouts(Connector):
         self.opsdroid = None
         self.args = None
         self.cookies = hangups.auth.get_auth_stdin('')
-        # self.cookies = hangups.auth.get_auth_stdin('/home/gabeduke/.local/share/hangupsbot/cookies.json')
         self.client = hangups.Client(self.cookies)
 
     async def connect(self, opsdroid=None):
@@ -41,19 +33,19 @@ class ConnectorHangouts(Connector):
         _LOGGER.info("Connecting to Hangouts")
 
         try:
-            connection = await self.client.connect()
+            await self.client.connect()
         except aiohttp.ClientOSError as error:
             _LOGGER.error(error)
             _LOGGER.error("Failed to connect to Hangouts")
 
     async def listen(self, opsdroid):
-        _LOGGER.info('loading conversation list...')
-        user_list, conv_list = (
-            await hangups.build_user_conversation_list(self.client)
-        )
+        """Listen for and parse new messages."""
+        pass
 
     async def respond(self, opsdroid):
+        """Get the next message from the websocket."""
         pass
 
     async def disconnect(self, opsdroid):
+        """disconnect from chat service"""
         self.client.disconnect()
