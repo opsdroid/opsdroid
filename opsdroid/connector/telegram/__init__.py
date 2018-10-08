aa5d692b05eca4506d9b3f3305502673a24219bd
FabioRosado <fabioglrosado@gmail.com>
2018-10-08 12:31:45 +0100

    Refactor Telegram connector, update tests

--- opsdroid/connector/telegram/__init__.py
+++ opsdroid/connector/telegram/__init__.py
@@ -27,8 +27,9 @@ class ConnectorTelegram(Connector):
         self.name = "telegram"
         self.latest_update = None
         self.default_room = None
+        self.listening = True
         self.default_user = config.get("default-user", None)
-        self.whitelisted_users = config.get("whitelisted_users", None)
+        self.whitelisted_users = config.get("whitelisted-users", None)
         self.update_interval = config.get("update_interval", 1)
 
         try:
@@ -74,6 +75,77 @@ class ConnectorTelegram(Connector):
                 _LOGGER.debug("Connected to telegram as %s",
                               json["result"]["username"])
 
+    async def _parse_message(self, opsdroid, response):
+        """Handle logic to parse a received message.
+
+        Since everyone can send a private message to any user/bot
+        in Telegram, this method allows to set a list of whitelisted
+        users that can interact with the bot. If any other user tries
+        to interact with the bot the command is not parsed and instead
+        the bot will inform that user that he is not allowed to talk
+        with the bot.
+
+        We also set self.latest_update to +1 in order to get the next
+        available message (or an empty {} if no message has been received
+        yet) with the method self._get_messages().
+
+        Args:
+            opsdroid (OpsDroid): An instance of opsdroid core.
+            response (dict): Response returned by aiohttp.ClientSession.
+        """
+        if response["message"]["text"]:
+            user = response["message"]["from"]["username"]
+
+            message = Message(
+                response["message"]["text"],
+                user,
+                response["message"]["chat"],
+                self)
+
+            if not self.whitelisted_users or \
+                    user in self.whitelisted_users:
+                await opsdroid.parse(message)
+            else:
+                message.text = "Sorry, you're not allowed " \
+                               "to speak with this bot."
+                await self.respond(message)
+            self.latest_update = response["update_id"] + 1
+
+    async def _get_messages(self, opsdroid):
+        """Connect to the Telegram API.
+
+        Uses an aiohttp ClientSession to connect to Telegram API
+        and get the latest messages from the chat service.
+
+        The data["offset"] is used to consume every new message, the API
+        returns an  int - "update_id" value. In order to get the next
+        message this value needs to be increased by 1 the next time
+        the API is called. If no new messages exists the API will just
+        return an empty {}.
+
+        Args:
+            opsdroid (OpsDroid): An instance of opsdroid core.
+
+        """
+        async with aiohttp.ClientSession() as session:
+            data = {}
+            if self.latest_update is not None:
+                data["offset"] = self.latest_update
+            resp = await session.get(self.build_url("getUpdates"),
+                                     params=data)
+            if resp.status != 200:
+                _LOGGER.error("Telegram error %s, %s",
+                              resp.status, resp.text)
+                self.listening = False
+
+            else:
+                json = await resp.json()
+                # _LOGGER.debug(json)
+
+                for response in json["result"]:
+                    _LOGGER.debug(response)
+                    await self._parse_message(opsdroid, response)
+
     async def listen(self, opsdroid):
         """Listen for and parse new messages.
 
@@ -91,41 +163,8 @@ class ConnectorTelegram(Connector):
             opsdroid (OpsDroid): An instance of opsdroid core.
 
         """
-        while True:
-            async with aiohttp.ClientSession() as session:
-                data = {}
-                if self.latest_update is not None:
-                    data["offset"] = self.latest_update
-                resp = await session.post(self.build_url("getUpdates"),
-                                          params=data)
-                if resp.status != 200:
-                    _LOGGER.error("Telegram error %s, %s",
-                                  resp.status, resp.text)
-                    break
-
-                json = await resp.json()
-                # _LOGGER.debug(json)
-
-                for response in json["result"]:
-                    _LOGGER.debug(response)
-
-                    if response["message"]["text"]:
-                        user = response["message"]["from"]["username"]
-
-                        message = Message(
-                            response["message"]["text"],
-                            user,
-                            response["message"]["chat"],
-                            self)
-
-                        if not self.whitelisted_users or \
-                                user in self.whitelisted_users:
-                            await opsdroid.parse(message)
-                        else:
-                            message.text = "Sorry, you're not allowed " \
-                                           "to speak with this bot."
-                            await self.respond(message)
-                        self.latest_update = response["update_id"] + 1
+        while self.listening:
+            await self._get_messages(opsdroid)
 
             await asyncio.sleep(self.update_interval)
 
--- tests/test_connector_telegram.py
+++ tests/test_connector_telegram.py
@@ -27,10 +27,8 @@ class TestConnectorTelegram(unittest.TestCase):
 
     def test_missing_token(self):
         """Test that attempt to connect without info raises an error."""
-        with mock.patch('opsdroid.connector.telegram._LOGGER.error') \
-                as logmock:
-            ConnectorTelegram({})
-            self.assertTrue(logmock.called)
+        ConnectorTelegram({})
+        self.assertLogs('_LOGGER', 'error')
 
 
 class TestConnectorTelegramAsync(asynctest.TestCase):
@@ -39,7 +37,8 @@ class TestConnectorTelegramAsync(asynctest.TestCase):
     def setUp(self):
         self.connector = ConnectorTelegram({
                 'name': 'telegram',
-                'token': 'test',
+                'token': 'bot:765test',
+                'whitelisted-users': ['user', 'test']
             })
 
     async def test_connect(self):
@@ -57,14 +56,12 @@ class TestConnectorTelegramAsync(asynctest.TestCase):
         }
 
         with OpsDroid() as opsdroid, \
-            amock.patch('aiohttp.ClientSession.get') as patched_request, \
-            amock.patch('opsdroid.connector.telegram._LOGGER.debug',) \
-                as logmock:
+            amock.patch('aiohttp.ClientSession.get') as patched_request:
 
             patched_request.return_value = asyncio.Future()
             patched_request.return_value.set_result(connect_response)
             await self.connector.connect(opsdroid)
-            self.assertTrue(logmock.called)
+            self.assertLogs('_LOGGER', 'debug')
             self.assertNotEqual(200, patched_request.status)
             self.assertTrue(patched_request.called)
 
@@ -73,55 +70,149 @@ class TestConnectorTelegramAsync(asynctest.TestCase):
         result.status = 401
 
         with OpsDroid() as opsdroid, \
-            amock.patch('aiohttp.ClientSession.get') as patched_request, \
-            amock.patch('opsdroid.connector.telegram._LOGGER.error',) \
-                as logmock:
+            amock.patch('aiohttp.ClientSession.get') as patched_request:
 
             patched_request.return_value = asyncio.Future()
             patched_request.return_value.set_result(result)
 
             await self.connector.connect(opsdroid)
-            self.assertTrue(logmock.called)
+            self.assertLogs('_LOGGER', 'error')
 
-    # async def test_listen_loop(self):
-    #     listen_response = amock.Mock()
-    #     listen_response.status = 200
-    #     listen_response.json = amock.CoroutineMock()
-    #     listen_response.return_value =
-    #
-    #     with OpsDroid() as opsdroid, \
-    #         amock.patch('aiohttp.ClientSession.get') as patched_request, \
-    #         amock.patch('opsdroid.connector.telegram._LOGGER.debug',) \
-    #             as logmock:
-    #
-    #         patched_request.return_value = asyncio.Future()
-    #         patched_request.return_value.set_result(listen_response)
-    #         await self.connector.listen(opsdroid)
-    #         self.assertTrue(patched_request.called)
-    #         self.assertTrue(logmock.called)
+    async def test_parse_message(self):
+        response = {
+            "update_id": 427647860,
+            "message": {
+                "message_id": 12,
+                "from": {
+                    "id": 649671308,
+                    "is_bot": False,
+                    "first_name": "A",
+                    "last_name": "User",
+                    "username": "user",
+                    "language_code": "en-GB"
+                },
+                "chat": {
+                    "id": 649671308,
+                    "first_name": "A",
+                    "last_name": "User",
+                    "username": "user",
+                    "type": "private"
+                },
+                "date": 1538756863,
+                "text": "Hello"
+            }
+        }
 
-    async def test_listen_loop_failure(self):
+        with OpsDroid() as opsdroid, \
+                amock.patch('opsdroid.core.OpsDroid.parse') as mocked_parse:
+            await self.connector._parse_message(opsdroid, response)
+            self.assertTrue(mocked_parse.called)
+
+    async def test_parse_message_unauthorized(self):
+        self.connector.config['whitelisted-users'] = ['user', 'test']
+        response = {
+            "update_id": 427647860,
+            "message": {
+                "message_id": 12,
+                "from": {
+                    "id": 649671308,
+                    "is_bot": False,
+                    "first_name": "A",
+                    "last_name": "User",
+                    "username": "a_user",
+                    "language_code": "en-GB"
+                },
+                "chat": {
+                    "id": 649671308,
+                    "first_name": "A",
+                    "last_name": "User",
+                    "username": "a_user",
+                    "type": "private"
+                },
+                "date": 1538756863,
+                "text": "Hello"
+            }
+        }
+
+        self.assertEqual(
+            self.connector.config['whitelisted-users'], ['user', 'test'])
+
+        message_text = "Sorry, you're not allowed to speak with this bot."
+
+        with OpsDroid() as opsdroid, \
+                amock.patch.object(self.connector, 'respond') \
+                as mocked_respond:
+            await self.connector._parse_message(opsdroid, response)
+            self.assertTrue(mocked_respond.called)
+            self.assertTrue(mocked_respond.called_with(message_text))
+
+    async def test_get_messages(self):
+        listen_response = amock.Mock()
+        listen_response.status = 200
+        listen_response.json = amock.CoroutineMock()
+        listen_response.return_value = {"result": [
+            {
+                "update_id": 427647860,
+                "message": {
+                    "message_id": 54,
+                    "from": {
+                        "id": 639889348,
+                        "is_bot": False,
+                        "first_name": "Fabio",
+                        "last_name": "Rosado",
+                        "username": "FabioRosado",
+                        "language_code": "en-GB"
+                    },
+                    "chat": {
+                        "id": 639889348,
+                        "first_name": "Fabio",
+                        "last_name": "Rosado",
+                        "username": "FabioRosado",
+                        "type": "private"
+                    },
+                    "date": 1538756863,
+                    "text": "Hello"
+                }
+            }
+        ]}
+
+        with OpsDroid() as opsdroid, \
+            amock.patch('aiohttp.ClientSession.get') as patched_request, \
+            amock.patch.object(self.connector, '_parse_message') \
+                as mocked_parse_message:
+
+            self.connector.latest_update = 54
+
+            patched_request.return_value = asyncio.Future()
+            patched_request.return_value.set_result(listen_response)
+            await self.connector._get_messages(opsdroid)
+            self.assertTrue(patched_request.called)
+            self.assertLogs('_LOGGER', 'debug')
+            # self.assertTrue(mocked_parse_message.called)
+
+    async def test_get_messages_failure(self):
         listen_response = amock.Mock()
         listen_response.status = 401
 
         with OpsDroid() as opsdroid, \
-            amock.patch('aiohttp.ClientSession.get') as patched_request, \
-            amock.patch('opsdroid.connector.telegram._LOGGER.error',) \
-                as logmock:
+            amock.patch('aiohttp.ClientSession.get') as patched_request:
 
             patched_request.return_value = asyncio.Future()
             patched_request.return_value.set_result(listen_response)
-            await self.connector.listen(opsdroid)
-            self.assertTrue(logmock.called)
+            await self.connector._get_messages(opsdroid)
+            self.assertLogs('_LOGGER', 'error')
+
+    async def test_listen(self):
+        self.connector.listening = amock.CoroutineMock()
+        self.connector.listening.side_effect = Exception()
+        await self.connector.listen(amock.CoroutineMock())
 
     async def test_respond(self):
         post_response = amock.Mock()
         post_response.status = 200
 
         with OpsDroid() as opsdroid, \
-            amock.patch('aiohttp.ClientSession.post') as patched_request, \
-            amock.patch('opsdroid.connector.telegram._LOGGER.debug') \
-                as logmock:
+            amock.patch('aiohttp.ClientSession.post') as patched_request:
 
             self.assertTrue(opsdroid.__class__.instances)
             test_message = Message(text="This is a test",
@@ -133,16 +224,14 @@ class TestConnectorTelegramAsync(asynctest.TestCase):
             patched_request.return_value.set_result(post_response)
             await test_message.respond("Response")
             self.assertTrue(patched_request.called)
-            self.assertTrue(logmock.called)
+            self.assertLogs("_LOGGER", "debug")
 
     async def test_respond_failure(self):
         post_response = amock.Mock()
         post_response.status = 401
 
         with OpsDroid() as opsdroid, \
-            amock.patch('aiohttp.ClientSession.post') as patched_request, \
-            amock.patch('opsdroid.connector.telegram._LOGGER.debug') \
-                as logmock:
+            amock.patch('aiohttp.ClientSession.post') as patched_request:
 
             self.assertTrue(opsdroid.__class__.instances)
             test_message = Message(text="This is a test",
@@ -153,5 +242,4 @@ class TestConnectorTelegramAsync(asynctest.TestCase):
             patched_request.return_value = asyncio.Future()
             patched_request.return_value.set_result(post_response)
             await test_message.respond("Response")
-            self.assertTrue(logmock.called)
-
+            self.assertLogs('_LOGGER', 'debug')
