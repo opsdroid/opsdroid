792fe920f8bbf92c376f5f5d95a0a46450c3a463
David Morrison <drmorr@yelp.com>
2018-11-23 17:21:22 -0800

    added a test and disconnect method

--- opsdroid/connector/slack/__init__.py
+++ opsdroid/connector/slack/__init__.py
@@ -63,7 +63,7 @@ class ConnectorSlack(Connector):
             _LOGGER.error("Failed to connect to Slack, retrying in 10")
             await self.reconnect(10)
         except Exception:
-            await self.slacker.close()
+            await self.disconnect()
             raise
 
     async def reconnect(self, delay=None):
@@ -75,6 +75,10 @@ class ConnectorSlack(Connector):
             await self.connect()
         finally:
             self.reconnecting = False
+
+    async def disconnect(self, opsdroid=None):
+        """Disconnect from Slack."""
+        await self.slacker.close()
 
     async def listen(self, opsdroid):
         """Listen for and parse new messages."""
