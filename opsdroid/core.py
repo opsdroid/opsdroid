8ec7487773cd27de8978fae6dc3a9487c355c311
Fábio Rosado <fabioglrosado@gmail.com>
2018-12-21 20:35:44 +0000

    Cancel pending tasks on unload (#729)
    Solves "task was cancelled but was pending" error.

--- opsdroid/core.py
+++ opsdroid/core.py
@@ -185,6 +185,13 @@ class OpsDroid():
         self.cron_task = None
         _LOGGER.info(_("Stopped cron"))
 
+        _LOGGER.info(_("Stopping pending tasks..."))
+        tasks = asyncio.Task.all_tasks()
+        for task in list(tasks):
+            if not task.done() and task is not asyncio.Task.current_task():
+                task.cancel()
+        _LOGGER.info(_("Stopped pending tasks"))
+
     async def reload(self):
         """Reload opsdroid."""
         await self.unload()
