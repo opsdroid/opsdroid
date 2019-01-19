95a4fad305a842121656b453bb777879b813e2df
Drew Leonard <andy.j.leonard@gmail.com>
2019-01-19 15:28:11 +0000

    Reinstate opsdroid.message.Message to support old usage

---
+++ opsdroid/message.py
@@ -0,0 +1,14 @@
+"""Support for deprecated opsdroid.message.Message"""
+
+from warnings import warn, DeprecationWarning
+
+from opsdroid.events import Message as NewMessage
+
+
+warn("opsdroid.message.Message is deprecated. "
+     "Please use opsdroid.events.Message instead.",
+     DeprecationWarning)
+
+
+def Message(text, user, room, connector, raw_message=None):
+    return NewMessage(user, room, connector, text, raw_message)
