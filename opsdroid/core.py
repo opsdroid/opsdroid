8332c99904e5fbc413e997b8c6331c775df3ee3f
Jacob Tomlinson <jacob@tom.linson.uk>
2019-01-28 08:34:35 +0000

    Better argument inspection

--- opsdroid/core.py
+++ opsdroid/core.py
@@ -8,6 +8,7 @@ import sys
 import weakref
 import asyncio
 import contextlib
+import inspect
 
 from opsdroid.const import DEFAULT_CONFIG_PATH
 from opsdroid.memory import Memory
@@ -312,12 +313,10 @@ class OpsDroid():
         # halt the application. If a skill throws an exception it just doesn't
         # give a response to the user, so an error response should be given.
         try:
-            await skill(message)
-        except TypeError as error:
-            if 'positional arguments' in str(error):
+            if len(inspect.getargspec(skill)[0]) > 1:
                 await skill(self, config, message)
             else:
-                raise error
+                await skill(message)
         except Exception:
             if message:
                 await message.respond(_("Whoops there has been an error"))
