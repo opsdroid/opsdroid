a64cbfa1a8db10dfe8f5d4f51a06c57710e123d9
Hicham Terkiba <hicham@terkiba.com>
2019-03-20 10:57:59 +0100

    Fixing rasanlu intents matchers issue #860

--- opsdroid/parsers/rasanlu.py
+++ opsdroid/parsers/rasanlu.py
@@ -17,7 +17,7 @@ _LOGGER = logging.getLogger(__name__)
 async def _get_all_intents(skills):
     """Get all skill intents and concatenate into a single markdown string."""
     intents = [skill["intents"] for skill in skills
-                if skill["intents"] is not None]
+               if skill["intents"] is not None]
     if not intents:
         return None
     intents = "\n\n".join(intents)
