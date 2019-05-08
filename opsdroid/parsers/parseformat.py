7ec1ab2af6960a48d728c0aa1416123ec0bc4f27
anxodio <angelfernandezibanez@gmail.com>
2019-05-08 12:17:23 +0200

    Add parse_format matcher (#922)

---
+++ opsdroid/parsers/parseformat.py
@@ -0,0 +1,43 @@
+"""A helper function for parsing and executing parse_format skills."""
+
+import logging
+import copy
+
+from parse import parse, search
+
+from opsdroid.parsers.regex import calculate_score
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def match_format(text, opts):
+    """Match a message considering the options."""
+    case_sensitive = opts["case_sensitive"]
+
+    match_fn = parse
+    if opts["matching_condition"].lower() == "search":
+        match_fn = search
+
+    return match_fn(opts["expression"], text, case_sensitive=case_sensitive)
+
+
+async def parse_format(opsdroid, skills, message):
+    """Parse a message against all parse_format skills."""
+    matched_skills = []
+    for skill in skills:
+        for matcher in skill.matchers:
+            if "parse_format" in matcher:
+                opts = matcher["parse_format"]
+                result = await match_format(message.text, opts)
+                if result:
+                    new_message = copy.copy(message)
+                    new_message.parse_result = result
+                    matched_skills.append({
+                        "score": await calculate_score(
+                            opts["expression"], opts["score_factor"]),
+                        "skill": skill,
+                        "config": skill.config,
+                        "message": new_message
+                    })
+    return matched_skills
