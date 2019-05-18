e69e9ffcf6ed013203c03acd8a19c27ba3495121
Razi <razi@administrators-MacBook-Pro-108.local>
2019-05-19 01:25:32 +0530

    working stream

--- opsdroid/connector/gitter/__init__.py
+++ opsdroid/connector/gitter/__init__.py
@@ -1,6 +1,7 @@
 
 import logging
 import aiohttp
+import asyncio
 
 
 
@@ -32,27 +33,28 @@ class ConnectorGitter(Connector):
     _LOGGER.debug("Connecting with gitter stream")
     self.session = aiohttp.ClientSession()
     self.response = await self.session.get("https://stream.gitter.im/v1/rooms/5a572a4dd73408ce4f87910d/chatMessages?access_token=d404d5500ace6a0ad7b2c0cf9cfc586c80529e30")
-    # self.response = yield aiohttp.request('get',"https://stream.gitter.im/v1/rooms/5a572a4dd73408ce4f87910d/chatMessages?access_token=d404d5500ace6a0ad7b2c0cf9cfc586c80529e30")
 
   async def read_response(self):
     _LOGGER.debug("Connecting with gitter stream")
 
-
-
   async def listen(self):
-    _LOGGER.debug("Connecting with gitter stream")
-    async for line in self.response.content:
-      print(line)
+    _LOGGER.debug("Listening with gitter stream")
+    while True:
+      await asyncio.sleep(self.update_interval)
+      async for data in self.response.content.iter_chunked(1024):
+        _LOGGER.info(data)
 
-    # Listen for new messages from the chat service
+
+
 
 
 
 
   async def respond(self, message):
     # Send message.text back to the chat service
-    await ""
+    n = ""
 
   async def disconnect(self):
     # Disconnect from the chat service
-    await ""
+    self.response
+
