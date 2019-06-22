b24e18fb0645e0e1968c04aafd11ae88dc92b7a2
kollyuchka <kollyuchkaola@gmail.com>
2019-06-22 17:12:41 +0600

    add validation to configuration

---
+++ opsdroid/configuration/validation.py
@@ -0,0 +1,10 @@
+# Import Yamale and make a schema object, make sure ruamel.yaml is installed already.
+import yamale
+schema = yamale.make_schema('shema.yaml')
+
+# Create a Data object
+data = yamale.make_data('example1_configuration.yaml')
+
+# Validate data against the schema same as before.
+yamale.validate(schema, data)
+
