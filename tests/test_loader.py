37b496d8300af81338779d503ad9ebb951a92b7e
kollyuchka <kollyuchkaola@gmail.com>
2019-07-09 14:51:37 +0600

    added test for config without db and parsers

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -56,6 +56,13 @@ class TestLoader(unittest.TestCase):
         )
         self.assertIsNotNone(config)
 
+    def test_load_config_valid_without_db_and_parsers(self):
+        opsdroid, loader = self.setup()
+        config = loader.load_config_file(
+            [os.path.abspath("tests/configs/valid_without_db_and_parsers.yaml")]
+        )
+        self.assertIsNotNone(config)
+
     def test_load_config_broken(self):
         opsdroid, loader = self.setup()
 
