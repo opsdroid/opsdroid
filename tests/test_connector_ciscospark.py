98f4fcd61d84fce9378e649d8748fbac8999ca8d
himanshupathak21061998 <hpathak336@gmail.com>
2019-08-18 00:42:25 +0530

    Adding test for connector

--- tests/test_connector_ciscospark.py
+++ tests/test_connector_ciscospark.py
@@ -5,6 +5,7 @@ import unittest
 import unittest.mock as mock
 import asynctest
 import asynctest.mock as amock
+from ciscosparkapi import CiscoSparkAPI
 
 from opsdroid.core import OpsDroid
 from opsdroid.connector.ciscospark import ConnectorCiscoSpark
@@ -29,3 +30,27 @@ class TestConnectorCiscoSpark(unittest.TestCase):
         """Test that creating without an API without config raises an error."""
         with self.assertRaises(TypeError):
             ConnectorCiscoSpark()
+
+class TestConnectorCiscoSparkAsync(asynctest.TestCase):
+    """Test the async methods of the opsdroid Cisco Spark connector class."""
+
+    async def setUp(self):
+        configure_lang({})
+
+    async def test_connect(self):
+        connector = ConnectorCiscoSpark({"access-token": "abc123"})
+
+        opsdroid = amock.CoroutineMock()
+        opsdroid.eventloop = self.loop
+        connector.clean_up_webhooks = amock.CoroutineMock()
+        connector.subscribe_to_rooms = amock.CoroutineMock()
+        connector.set_own_id = amock.CoroutineMock()
+
+        with amock.patch(
+            "websockets.connect", new=amock.CoroutineMock()
+        ) as mocked_websocket_connect:
+            await connector.connect(opsdroid=OpsDroid())
+
+        self.assertTrue(connector.clean_up_webhooks.called)
+        self.assertTrue(connector.subscribe_to_rooms.called)
+        self.assertTrue(connector.set_own_id.called)
