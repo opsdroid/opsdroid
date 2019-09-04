5fccecd47fe8846fe78e0daa27eb216f06f40192
FabioRosado <fabioglrosado@gmail.com>
2019-09-04 19:09:30 +0100

    Update docstrings

--- opsdroid/parsers/dialogflow.py
+++ opsdroid/parsers/dialogflow.py
@@ -11,7 +11,23 @@ _LOGGER = logging.getLogger(__name__)
 
 
 async def call_dialogflow(text, config):
-    """Call the Dialogflow api and return the response."""
+    """Call Dialogflow to get intent from text.
+
+    Dialogflow will return an object with a few restrictions, you can't
+    iterate it and the only way to access each element is by using dot
+    notation.
+
+    Args:
+        text (string): message.text this is the text obtained from the user.
+        config (dict): configuration settings from the file config.yaml
+
+    Return:
+        A 'google.cloud.dialogflow_v2.types.DetectIntentResponse' object.
+
+    Raises:
+        Warning: if Google credentials are not found in environmental
+        variables or 'project-id' is not in config.
+    """
 
     if os.environ["GOOGLE_APPLICATION_CREDENTIALS"] and config.get("project-id"):
         session_client = dialogflow.SessionsClient()
@@ -35,7 +51,28 @@ async def call_dialogflow(text, config):
 
 
 async def parse_dialogflow(opsdroid, skills, message, config):
-    """Parse a message against all Dialogflow skills."""
+    """Parse a message against all Dialogflow skills.
+
+    This function does a few things, first it will check if the
+    intent confidence is higher than the minimum score set on config,
+    then it will try to match an action or an intent to a matcher and
+    add the proper skills to the skills list.
+
+    At the moment a broad exception is being used due to the fact that
+    dialogflow library doesn't have the best documentation yet and it's
+    not possible to know what sort of exceptions the library will return.
+
+    Args:
+        opsdroid (OpsDroid): An instance of opsdroid.core.
+        skills (list): A list containing all skills available.
+        message(object): An instance of events.message.
+        config (dict): configuration settings from the
+            file config.yaml.
+
+    Return:
+        Either empty list or a list containing all matched skills.
+
+    """
     try:
         result = await call_dialogflow(message.text, config)
         matched_skills = []
@@ -73,6 +110,7 @@ async def parse_dialogflow(opsdroid, skills, message, config):
         return matched_skills
 
     except Exception as error:
+        # TODO: Refactor broad exception
         _LOGGER.error(
             "There was an error while parsing to dialogflow - {}".format(error)
         )
