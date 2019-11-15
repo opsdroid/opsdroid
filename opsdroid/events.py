e1d536597cd60edf860d1354a4ccb06d7a8f5931
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2019-11-15 18:53:46 +0000

    Remove whitespace

--- opsdroid/events.py
+++ opsdroid/events.py
@@ -214,7 +214,7 @@ class Message(Event):
     async def _thinking_delay(self):
         """Make opsdroid wait x-seconds before responding.
 
-        Number of seconds defined in YAML config. file, accessed via connector.      
+        Number of seconds defined in YAML config. file, accessed via connector.
 
         """
         seconds = self.connector.configuration.get("thinking-delay", 0)
@@ -231,8 +231,8 @@ class Message(Event):
         number of seconds defined in YAML config. file, accessed via connector.
 
         Args:
-            text (str): The text input to perform typing simulation on.  
-            
+            text (str): The text input to perform typing simulation on.
+
         """
         seconds = self.connector.configuration.get("typing-delay", 0)
         char_count = len(text)
