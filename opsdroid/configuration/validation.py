7e28647da1ae093c8cb3cb8f609d6aaec1502492
FabioRosado <fabioglrosado@gmail.com>
2019-11-18 09:56:31 +0000

    Add url check

--- opsdroid/configuration/validation.py
+++ opsdroid/configuration/validation.py
@@ -1,99 +1,237 @@
 """Schema validation for configuration.yaml."""
 
-from voluptuous import Schema, ALLOW_EXTRA, Optional, Required, Url
+from voluptuous import Schema, ALLOW_EXTRA, Optional, Required, Url, Any
 
-# parsers_config = {
-#     "name": str,
-#     "enabled": bool,
-#     "token": str,
-#     "appid": str,
-#     "appkey": str,
-#     "verbose": bool,
-#     "min-score": float,
-# }
+parsers = Any(
+    None,
+    {
+        Optional("dialogflow"): {
+            Required(
+                "project-id",
+                msg="Dialogflow parser required param 'project-id' not provided",
+            ): str,
+            Optional("min-score"): float,
+        },
+        Optional("luisai"): {
+            Required(
+                "appid", msg="Luis.ai Parser required param 'appid' not provided"
+            ): str,
+            Required(
+                "appkey", msg="Luis.ai Parser required param 'appkey' not provided"
+            ): str,
+            Required(
+                "verbose", msg="Luis.ai Parser required param 'verbose' not provided"
+            ): bool,
+            Optional("min-score"): float,
+        },
+        Optional("sapcai"): {
+            Required(
+                "token", msg="SAPCAI Parser required param 'token' not provided"
+            ): str,
+            Optional("min-score"): float,
+        },
+        Optional("watson"): {
+            Required(
+                "gateway", msg="Watson Parser required param 'gateway' not provided"
+            ): str,
+            Required(
+                "assistant-id",
+                msg="Watson Parser required param 'assistant-id' not provided",
+            ): str,
+            Required(
+                "token", msg="Watson Parser required param 'token' not provided"
+            ): str,
+            Optional("min-score"): float,
+        },
+        Optional("witai"): {
+            Required(
+                "token", msg="Witai Parser required param 'token' not provided"
+            ): str,
+            Optional("min-score"): float,
+        },
+        Optional("rasanlu", default=dict): Any(
+            None,
+            {
+                Optional("url"): str,
+                Optional("project"): str,
+                Optional("token"): str,
+                Optional("min-score"): float,
+            },
+        ),
+        Optional("regex", default=dict): Any(None, {Optional("enabled"): bool}),
+        Optional("crontab", default=dict): Any(None, {Optional("enabled"): bool}),
+        Optional("parse_format", default=dict): Any(None, {Optional("enabled"): bool}),
+    },
+)
 
-parsers_config = {
-    "name": str,
-    "enabled": bool,
-    "token": str,
-    "appid": str,
-    "appkey": str,
-    "verbose": bool,
-    "min-score": float,
+connectors = {
+    Optional("websocket"): Any(
+        None,
+        {
+            Optional("bot-name"): str,
+            Optional("max-connections"): int,
+            Optional("connection-timeout"): int,
+        },
+    ),
+    Optional("ciscospark"): {
+        Required(
+            "webhook-url",
+            msg="CiscoSpark Connector required param 'webhook-url' not provided",
+        ): Url,
+        Required(
+            "token", msg="CiscoSpark Connector required param 'token' not provided"
+        ): str,
+    },
+    Optional("twitter"): {
+        Required(
+            "consumer_key",
+            msg="Twitter Connector required param 'consumer_key' not provided",
+        ): str,
+        Required(
+            "consumer_secret",
+            msg="Twitter Connector required param 'consumer_secret' not provided",
+        ): str,
+        Required(
+            "oauth_token",
+            msg="Twitter Connector required param 'oath_token' not provided",
+        ): str,
+        Required(
+            "oauth_token_secret",
+            msg="Twitter Connector required param 'oath_token_secret' not provided",
+        ): str,
+        Optional("enable_dms"): bool,
+        Optional("enable_tweets"): bool,
+    },
+    Optional("webexteams"): {
+        Required(
+            "webhook-url",
+            msg="Webexteams Connector required param 'webhook-url' not provided",
+        ): Url,
+        Required(
+            "token", msg="Webexteams Connector required param 'token' not provided"
+        ): str,
+    },
+    Optional("facebook"): {
+        Required(
+            "verify-token",
+            msg="Facebook Connector required param 'verify-token' not provided",
+        ): str,
+        Required(
+            "page-access-token",
+            msg="Facebook Connector required param 'page-access-token' not provided",
+        ): str,
+        Optional("bot-name"): str,
+    },
+    Optional("matrix"): {
+        Required(
+            "mxid", msg="Matrix Connector required param 'mxid' not provided"
+        ): str,
+        Required(
+            "password", msg="Matrix Connector required param 'password' not provided"
+        ): str,
+        Required(
+            "rooms", msg="Matrix Connector required param 'rooms' not provided"
+        ): dict,
+        Optional("homeserver"): str,
+        Optional("nick"): str,
+        Optional("room_specific_nicks"): bool,
+    },
+    Optional("mattermost"): {
+        Required(
+            "token", msg="Mattermost Connector required param 'token' not provided"
+        ): str,
+        Required(
+            "url", msg="Mattermost Connector required param 'url' not provided"
+        ): str,
+        Required(
+            "team-name",
+            msg="Mattermost Connector required param 'team-name' not provided",
+        ): str,
+        Optional("scheme"): str,
+        Optional("port"): int,
+        Optional("ssl-verify"): bool,
+        Optional("connect-timeout"): int,
+    },
+    Optional("github"): {
+        Required(
+            "token", msg="Github Connector required param 'token' not provided"
+        ): str
+    },
+    Optional("gitter"): {
+        Required(
+            "token", msg="Gitter Connector required param 'token' not provided"
+        ): str,
+        Required(
+            "room-id", msg="Gitter Connector required param 'room-id' not provided"
+        ): str,
+        Optional("bot-name"): str,
+    },
+    Optional("telegram"): {
+        Required(
+            "token", msg="Telegram Connector required param 'token' not provided"
+        ): str,
+        Optional("update-interval"): float,
+        Optional("default-user"): str,
+        Optional("whitelisted-users"): list,
+    },
+    Optional("slack"): {
+        Required(
+            "token", msg="Slack Connector required param 'token' not provided"
+        ): str,
+        Optional("bot-name"): str,
+        Optional("default-room"): str,
+        Optional("icon-emoji"): str,
+        Optional("connect-timeout"): int,
+        Optional("chat-as-user"): bool,
+    },
+    Optional("rocketchat"): {
+        Required(
+            "token", msg="Rocket.chat Connector required param 'token' not provided"
+        ): str,
+        Required(
+            "user-id", msg="Rocket.chat Connector required param 'user-id' not provided"
+        ): str,
+        Optional("bot-name"): str,
+        Optional("default-room"): str,
+        Optional("channel-url"): Url,
+        Optional("update-interval"): int,
+        Optional("group"): str,
+    },
+    Optional("shell"): Any(None, {Optional("bot-name"): str}),
 }
 
-connectors_config = {
-    "name": str,
-    "bot-name": str,
-    "max-connections": int,
-    "connection-timeout": int,
-    "webhook-url": Url(),
-    "consumer-key": str,
-    "consumer-secret": str,
-    "oauth-token": str,
-    "oauth-token-secret": str,
-    "enable_dms": str,
-    "verify-token": str,
-    "page-access-token": str,
-    "mxid": str,
-    "password": str,
-    "room": str,
-    "rooms": dict,
-    "homeserver": str,
-    "nick": str,
-    "room_specific_nicks": str,
-    "token": str,
-    "update-interval": int,
-    "default-user": str,
-    "whitelisted-users": list,
-    "default-room": str,
-    "icon-emoji": str,
-    "connection-timeout": int,
-    "user-id": str,
-    "group": str,
-    "channel-url": Url(),
-}
-
-
-# schema = {
-#     "logging": {"level": str, "console": bool},
-#     "welcome-message": bool,
-#     "connectors": [{"name": str, "token": str}],
-#     "skills": [{"name": str}],
-#     Optional("parsers", default=list): [parsers_config],
-#     Required("connectors", default=list): [connectors_config],
-#     # Optional("databases", default=list): [
-#     #     {
-#     #         "name": str,
-#     #         "host": str,
-#     #         "port": str,
-#     #         Optional("database", default=int): str,
-#     #         "password": str,
-#     #         "reconnect": bool,
-#     #         "file": str,
-#     #         "table": str,
-#     #     }
-#     # ],
-#     Optional("skills", default=list): [{"name": str}],
-# }
+databases = Any(
+    None,
+    {
+        Optional("redis"): Any(
+            None,
+            {
+                Optional("host"): str,
+                Optional("port"): Any(int, str),
+                Optional("database"): int,
+                Optional("password"): str,
+            },
+        ),
+        Optional("mongo"): Any(
+            None,
+            {
+                Optional("host"): str,
+                Optional("port"): Any(int, str),
+                Optional("database"): str,
+            },
+        ),
+        Optional("sqlite"): Any(None, {Optional("file"): str, Optional("table"): str}),
+    },
+)
 
 schema = {
     "logging": {"level": str, "console": bool},
+    "module-path": str,
     "welcome-message": bool,
-    "connectors": [{"name": str, "token": str, "access-token": str}],
-    "skills": [{"name": str}],
-    Optional("parsers", default=list): {
-        Optional("watson"): {
-            Required("gateway", msg="Watson Parser required param not provided"): str,
-            Required(
-                "assistant-id", msg="Watson Parser required param not provided"
-            ): str,
-            Required("token", msg="Watson Parser required param not provided"): str,
-        },
-        Optional("regex"): {
-            Required("enabled", msg="Regex Parser required param not provided"): bool
-        },
-    },
+    "web": {"host": str, "port": int, "ssl": {"cert": str, "key": str}},
+    "parsers": parsers,
+    "connectors": connectors,
+    "databases": databases,
 }
 
 
@@ -114,8 +252,7 @@ def validate_configuration(data):
         data: a yaml stream obtained from opening configuration.yaml
 
     """
-    print(data)
-    print(type(data["parsers"]["regex"]))
     validate = Schema(schema, extra=ALLOW_EXTRA)
+    print(data)
     print(validate)
     validate(data)
