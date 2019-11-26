252631adcf7da93c3a204c15c973dd76fa41942c
himanshupathak21061998 <hpathak336@gmail.com>
2019-11-27 02:02:50 +0530

    Adding basic structure without error checking to make parser and matcher function [skip ci]

--- opsdroid/parsers/snipsnlu.py
+++ opsdroid/parsers/snipsnlu.py
@@ -7,6 +7,8 @@ import unicodedata
 from hashlib import sha256
 
 import os
+from snips_nlu import SnipsNLUEngine
+from os import path
 import arrow
 
 from opsdroid.const import SNIPSNLU_DEFAULT_DIR, SNIPSNLU_DEFAULT_PROJECT
@@ -46,21 +48,73 @@ async def _init_model(config):
 
 
 async def _build_dir(config):
-    pass
-
+    directory = config.get("directory", SNIPSNLU_DEFAULT_DIR)
+    if path.exists(directory):
+        return directory
+    else:
+        os.system("mkdir "+directory)
+        return directory
+    
 async def _get_existing_models(config):
-    """Get a list of models already trained in the Snips NLU project."""
-    project = config.get("project", SNIPSNLU_DEFAULT_PROJECT)
+    """Get a list of models already trained in the Snips NLU."""
     try:
         resp = await _build_dir(config)
-        os.system("cd {}").format(resp)
-        result = os.popen('ls').read()
+        result = os.popen('ls '+resp).read()
         result = list(result.split("\n"))
-        if project in result:
-            project_models = result[project]
-            return project_models["available_models"]
+        if len(result) != 0:
+            return result
     except OSError as e:
         _LOGGER.error(_("Directory not build"))
         
     return []
+
+async def train_rasanlu(config, skills):
+    """Train a Snips NLU model based on the loaded skills."""
+    _LOGGER.info("Starting Snips NLU training.")
+    intents = await _get_all_intents(skills)
+    if intents is None:
+        _LOGGER.warning("No intents found, skipping training.")
+        return False
+
+    config["model"] = await _get_intents_fingerprint(intents)
+    if config["model"] in await _get_existing_models(config):
+        _LOGGER.info("This model already exists, skipping training...")
+        await _init_model(config)
+        return True
+    _LOGGER.info("Now training the model. This may take a while...")
+    directory = _build_dir(config)
+
+    training_start = arrow.now()
+    engine = SnipsNLUEngine(config)
+    engine.fit(intents)
+    engine.persist("{}\{}".format(directory, config["model"]))
+    time_taken = (arrow.now() - training_start).total_seconds()
+    _LOGGER.info("Rasa NLU training completed in %s seconds.",int(time_taken))
+    return True
         
+async def call_snipsnlu(text, config):
+    """Call the Snips NLU api and return the response."""
+    model = config.get("model", "fallback")
+    directory = _build_dir()
+    
+    loaded_engine = SnipsNLUEngine.from_path("{}\{}".format(directory, model))
+    result = loaded_engine.parse(text)
+    return result
+
+async def parse_snipsnlu(opsdroid, skills, message, config):
+    """Parse a message against all Snips NLU skills."""
+    matched_skills = []
+    
+    result = await call_snipsnlu(message.text, config)
+    if result:
+        for skill in opsdroid.skills:
+            if "rasanlu_intent" in skill:
+                if skill['rasanlu_intent'] == result['intent']['name']:
+                    message.rasanlu = result
+                    matched_skills.append({
+                        "skill": skill["skill"],
+                        "config": skill["config"],
+                        "message": message
+                    })
+
+    return matched_skills
