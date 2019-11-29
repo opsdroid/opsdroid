29bb8363b2165c03b74271c5a6e49a66255394f5
daniccan <passion.dani@gmail.com>
2019-11-29 16:58:51 +0530

    Added new tests for interactive actions in Slack.

--- tests/test_connector_slack.py
+++ tests/test_connector_slack.py
@@ -68,9 +68,14 @@ class TestConnectorSlackAsync(asynctest.TestCase):
         opsdroid.eventloop = self.loop
         connector.slack_rtm._connect_and_read = amock.CoroutineMock()
         connector.slack.api_call = amock.CoroutineMock()
+        opsdroid.web_server = amock.CoroutineMock()
+        opsdroid.web_server.web_app = amock.CoroutineMock()
+        opsdroid.web_server.web_app.router = amock.CoroutineMock()
+        opsdroid.web_server.web_app.router.add_post = amock.CoroutineMock()
         await connector.connect()
         self.assertTrue(connector.slack_rtm._connect_and_read.called)
         self.assertTrue(connector.slack.api_call.called)
+        self.assertTrue(opsdroid.web_server.web_app.router.add_post.called)
 
     async def test_connect_auth_fail(self):
         connector = ConnectorSlack({"token": "abc123"}, opsdroid=OpsDroid())
@@ -243,3 +248,153 @@ class TestConnectorSlackAsync(asynctest.TestCase):
         connector.lookup_username.return_value = {"name": "user"}
         result = await connector.replace_usernames("Hello <@U023BECGF>!")
         self.assertEqual(result, "Hello user!")
+
+    async def test_block_actions_interactivity(self):
+        """Test the block_actions interactivity type in Slack interactions handler."""
+
+        connector = ConnectorSlack({"token": "abc123"}, opsdroid=OpsDroid())
+        connector.opsdroid = amock.CoroutineMock()
+        connector.opsdroid.parse = amock.CoroutineMock()
+
+        req_ob = {
+            "type": "block_actions",
+            "team": {"id": "T9TK3CUKW", "domain": "example"},
+            "user": {
+                "id": "UA8RXUSPL",
+                "username": "jtorrance",
+                "team_id": "T9TK3CUKW",
+            },
+            "channel": {"id": "CBR2V3XEX", "name": "review-updates"},
+            "actions": [
+                {
+                    "action_id": "WaXA",
+                    "block_id": "=qXel",
+                    "text": {"type": "plain_text", "text": "View", "emoji": True},
+                    "value": "click_me_123",
+                    "type": "button",
+                    "action_ts": "1548426417.840180",
+                }
+            ],
+        }
+
+        mock_request = amock.CoroutineMock()
+        mock_request.json = amock.CoroutineMock()
+        mock_request.json.return_value = req_ob
+
+        response = await connector.slack_interactions_handler(mock_request)
+        self.assertTrue(connector.opsdroid.parse.called)
+        self.assertEqual(type(response), aiohttp.web.Response)
+        self.assertEqual(response.status, 200)
+
+    async def test_message_action_interactivity(self):
+        """Test the message_action interactivity type in Slack interactions handler."""
+
+        connector = ConnectorSlack({"token": "abc123"}, opsdroid=OpsDroid())
+        connector.opsdroid = amock.CoroutineMock()
+        connector.opsdroid.parse = amock.CoroutineMock()
+
+        req_ob = {
+            "token": "Nj2rfC2hU8mAfgaJLemZgO7H",
+            "callback_id": "chirp_message",
+            "type": "message_action",
+            "trigger_id": "13345224609.8534564800.6f8ab1f53e13d0cd15f96106292d5536",
+            "response_url": "https://hooks.slack.com/app-actions/T0MJR11A4/21974584944/yk1S9ndf35Q1flupVG5JbpM6",
+            "team": {"id": "T0MJRM1A7", "domain": "pandamonium"},
+            "channel": {"id": "D0LFFBKLZ", "name": "cats"},
+            "user": {"id": "U0D15K92L", "name": "dr_maomao"},
+            "message": {
+                "type": "message",
+                "user": "U0MJRG1AL",
+                "ts": "1516229207.000133",
+                "text": "World's smallest big cat! <https://youtube.com/watch?v=W86cTIoMv2U>",
+            },
+        }
+
+        mock_request = amock.CoroutineMock()
+        mock_request.json = amock.CoroutineMock()
+        mock_request.json.return_value = req_ob
+
+        response = await connector.slack_interactions_handler(mock_request)
+        self.assertTrue(connector.opsdroid.parse.called)
+        self.assertEqual(type(response), aiohttp.web.Response)
+        self.assertEqual(response.status, 200)
+
+    async def test_view_submission_interactivity(self):
+        """Test the view_submission interactivity type in Slack interactions handler."""
+
+        connector = ConnectorSlack({"token": "abc123"}, opsdroid=OpsDroid())
+        connector.opsdroid = amock.CoroutineMock()
+        connector.opsdroid.parse = amock.CoroutineMock()
+
+        req_ob = {
+            "type": "view_submission",
+            "team": {"id": "T0MJRM1A7", "domain": "pandamonium"},
+            "user": {"id": "U0D15K92L", "name": "dr_maomao"},
+            "view": {
+                "id": "VNHU13V36",
+                "type": "modal",
+                "private_metadata": "shhh-its-secret",
+                "callback_id": "modal-with-inputs",
+                "state": {
+                    "values": {
+                        "multi-line": {
+                            "ml-value": {
+                                "type": "plain_text_input",
+                                "value": "This is my example inputted value",
+                            }
+                        }
+                    }
+                },
+                "hash": "156663117.cd33ad1f",
+            },
+        }
+
+        mock_request = amock.CoroutineMock()
+        mock_request.json = amock.CoroutineMock()
+        mock_request.json.return_value = req_ob
+
+        response = await connector.slack_interactions_handler(mock_request)
+        self.assertTrue(connector.opsdroid.parse.called)
+        self.assertEqual(type(response), aiohttp.web.Response)
+        self.assertEqual(response.status, 200)
+
+    async def test_view_closed_interactivity(self):
+        """Test the view_closed interactivity type in Slack interactions handler."""
+
+        connector = ConnectorSlack({"token": "abc123"}, opsdroid=OpsDroid())
+        connector.opsdroid = amock.CoroutineMock()
+        connector.opsdroid.parse = amock.CoroutineMock()
+
+        req_ob = {
+            "type": "view_closed",
+            "team": {"id": "TXXXXXX", "domain": "coverbands"},
+            "user": {"id": "UXXXXXX", "name": "dreamweaver"},
+            "view": {
+                "id": "VNHU13V36",
+                "type": "modal",
+                "private_metadata": "shhh-its-secret",
+                "callback_id": "modal-with-inputs",
+                "state": {
+                    "values": {
+                        "multi-line": {
+                            "ml-value": {
+                                "type": "plain_text_input",
+                                "value": "This is my example inputted value",
+                            }
+                        }
+                    }
+                },
+                "hash": "156663117.cd33ad1f",
+            },
+            "api_app_id": "AXXXXXX",
+            "is_cleared": false,
+        }
+
+        mock_request = amock.CoroutineMock()
+        mock_request.json = amock.CoroutineMock()
+        mock_request.json.return_value = req_ob
+
+        response = await connector.slack_interactions_handler(mock_request)
+        self.assertTrue(connector.opsdroid.parse.called)
+        self.assertEqual(type(response), aiohttp.web.Response)
+        self.assertEqual(response.status, 200)
