e28557f8b82a35e2198bea4709017fa238884e2b
Jacob Tomlinson <jtomlinson@nvidia.com>
2019-12-03 10:15:13 +0000

    Always run matched events

--- opsdroid/parsers/event_type.py
+++ opsdroid/parsers/event_type.py
@@ -10,32 +10,22 @@ _LOGGER = logging.getLogger(__name__)
 
 async def parse_event_type(opsdroid, event):
     """Parse an event if it's of a certain type."""
-    matched_skills = []
     for skill in opsdroid.skills:
         for matcher in skill.matchers:
             event_type = matcher.get("event_type", {}).get("type", None)
             if event_type:
                 # The event type can be specified with a string
                 if isinstance(event_type, str):
-                    # pylint: disable=invalid-name
-                    et = Event.event_registry.get(event_type, None)
-                    if et is None:
+                    matcher_event_type = Event.event_registry.get(event_type, None)
+                    if matcher_event_type is None:
                         raise ValueError(
                             "{event_type} is not a valid opsdroid"
                             " event representation.".format(event_type=event_type)
                         )
-                    event_type = et
+                    event_type = matcher_event_type
 
                 # TODO: Add option to match all subclasses as well
                 # if isinstance(event, event_type):
                 # pylint: disable=unidiomatic-typecheck
                 if type(event) is event_type:
-                    matched_skills.append(
-                        {
-                            "score": 1,
-                            "skill": skill,
-                            "config": skill.config,
-                            "message": event,
-                        }
-                    )
-    return matched_skills
+                    await opsdroid.run_skill(skill, skill.config, event)
