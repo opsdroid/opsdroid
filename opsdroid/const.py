6b6bb4ada3146bc6c44fff1f65aff46689ea7e9a
Fábio Rosado <fabioglrosado@gmail.com>
2020-08-10 13:14:16 +0100

    Add Twitch Connector  to opsdroid(#1497)

--- opsdroid/const.py
+++ opsdroid/const.py
@@ -44,3 +44,9 @@ SAPCAI_API_ENDPOINT = "https://api.cai.tools.sap/v2/request"
 WATSON_API_ENDPOINT = "https://{gateway}.watsonplatform.net/assistant/api"
 WATSON_API_VERSION = "2019-02-28"
 ENV_VAR_REGEX = r"^\"?\${?(?=\_?[A-Z])([A-Z-_]+)}?\"?$"
+
+TWITCH_OAUTH_ENDPOINT = "https://id.twitch.tv/oauth2/token"
+TWITCH_WEBHOOK_ENDPOINT = "https://api.twitch.tv/helix/webhooks/hub"
+TWITCH_API_ENDPOINT = "https://api.twitch.tv/helix"
+TWITCH_IRC_MESSAGE_REGEX = r"@.*;id=(?P<message_id>.*);m.*user-id=(?P<user_id>.*);user-type=.*:(?P<user>.*?)!.*PRIVMSG.*:(?P<message>.*)"
+TWITCH_JSON = os.path.join(DEFAULT_ROOT_PATH, "twitch.json")
