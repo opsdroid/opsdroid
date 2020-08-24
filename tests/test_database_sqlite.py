48aa01b4f97fd708df6a9af79ff4c33540340f7a
rr3khan <65298773+rr3khan@users.noreply.github.com>
2020-08-23 22:28:04 -0400

    Update test_database_sqlite.py

--- tests/test_database_sqlite.py
+++ tests/test_database_sqlite.py
@@ -1,115 +1,110 @@
-"""Tests for the DatabaseSqlite class."""
-import asyncio
-
-import unittest
-import asynctest
+"""Tests for DatabaseSqlite"""
+import pytest
 import asynctest.mock as amock
+
+import asyncio
 
 from opsdroid.database.sqlite import DatabaseSqlite
 from opsdroid.cli.start import configure_lang
 
-
-class TestDatabaseSqlite(unittest.TestCase):
-    """A database test class.
+"""A database test class.
 
     Test the opsdroid sqlite database class.
 
+"""
+configure_lang({})
+
+def test_init():
+    """Test initialisation of database class.
+
+    This method will test the initialisation of the database
+    class. It will assert if the database class properties are
+    declared.
+
     """
+    database = DatabaseSqlite({"path": "sqlite.db"})
+    assert database.client == None
+    assert database.db_file == "sqlite.db"
+    assert database.table == "opsdroid"
+    assert database.conn_args == {"isolation_level": None} 
 
-    def setUp(self):
-        self.loop = asyncio.new_event_loop()
-        configure_lang({})
-
-    def test_init(self):
-        """Test initialisation of database class.
-
-        This method will test the initialisation of the database
-        class. It will assert if the database class properties are
-        declared.
-
-        """
-        database = DatabaseSqlite({"path": "sqlite.db"})
-        self.assertEqual(None, database.client)
-        self.assertEqual("sqlite.db", database.db_file)
-        self.assertEqual("opsdroid", database.table)
-        self.assertEqual({"isolation_level": None}, database.conn_args)
-
-
-class TestDatabaseSqliteAsync(asynctest.TestCase):
     """A async database test class.
 
     Test the async methods of the opsdroid sqlite database class.
 
     """
+@pytest.mark.asyncio
+async def test_connect():
+    """Test database connection.
 
-    async def test_connect(self):
-        """Test database connection.
+    This method will test the database connection of sqlite database.
+    As the database is created `opsdroid` table is created first.
 
-        This method will test the database connection of sqlite database.
-        As the database is created `opsdroid` table is created first.
+    """
+    database = DatabaseSqlite({"path": "sqlite.db"})
+    opsdroid = amock.CoroutineMock()
+    opsdroid.eventloop = asyncio.new_event_loop()
 
-        """
-        database = DatabaseSqlite({"path": "sqlite.db"})
-        opsdroid = amock.CoroutineMock()
-        opsdroid.eventloop = self.loop
+    try:
+        await database.connect()
+        table = database.table
+        client = type(database.client).__name__
+        await database.disconnect()
+    except NotImplementedError:
+        raise Exception
+    else:
+        assert table == "opsdroid"
+        assert client == "Connection"
 
-        try:
-            await database.connect()
-            table = database.table
-            client = type(database.client).__name__
-            await database.disconnect()
-        except NotImplementedError:
-            raise Exception
-        else:
-            self.assertEqual("opsdroid", table)
-            self.assertEqual("Connection", client)
+@pytest.mark.asyncio
+async def test_disconnect():
+    """Test of database disconnection.
 
-    async def test_disconnect(self):
-        """Test of database disconnection.
+    This method will test the database disconnection of sqlite database.
 
-        This method will test the database disconnection of sqlite database.
+    """
+    database = DatabaseSqlite({"path": "sqlite.db"})
+    opsdroid = amock.CoroutineMock()
+    opsdroid.eventloop = asyncio.new_event_loop()
 
-        """
-        database = DatabaseSqlite({"path": "sqlite.db"})
-        opsdroid = amock.CoroutineMock()
-        opsdroid.eventloop = self.loop
+    try:
+        await database.connect()
+        await database.disconnect()
+    except NotImplementedError:
+        raise Exception
+    else:
+        pass
 
-        try:
-            await database.connect()
-            await database.disconnect()
-        except NotImplementedError:
-            raise Exception
-        else:
-            pass
+@pytest.mark.asyncio
+async def test_get_put_and_delete():
+    """Test get, put and delete functions of database
 
-    async def test_get_put_and_delete(self):
-        """Test get, put and delete functions of database
+    This method will test the get, put and delete functions which help to read
+    and write data from the database. The function `put` a value with
+    key and asserts the same value after the `get` operation is completed
+    followed by the `delete` operation which deletes the key.
 
-        This method will test the get, put and delete functions which help to read
-        and write data from the database. The function `put` a value with
-        key and asserts the same value after the `get` operation is completed
-        followed by the `delete` operation which deletes the key.
+    """
+    database = DatabaseSqlite({"path": "sqlite.db"})
+    opsdroid = amock.CoroutineMock()
+    opsdroid.eventloop = asyncio.new_event_loop()
 
-        """
-        database = DatabaseSqlite({"path": "sqlite.db"})
-        opsdroid = amock.CoroutineMock()
-        opsdroid.eventloop = self.loop
+    try:
+        await database.connect()
+        table = database.table
+        client = type(database.client).__name__
+        await database.put("hello", {})
+        data = await database.get("hello")
+        await database.delete("hello")
+        await database.disconnect()
+    except NotImplementedError:
+        raise Exception
+    else:
+        assert table == "opsdroid"
+        assert data == {}
+        assert client == "Connection"
 
-        try:
-            await database.connect()
-            table = database.table
-            client = type(database.client).__name__
-            await database.put("hello", {})
-            data = await database.get("hello")
-            await database.delete("hello")
-            await database.disconnect()
-        except NotImplementedError:
-            raise Exception
-        else:
-            self.assertEqual("opsdroid", table)
-            self.assertEqual({}, data)
-            self.assertEqual("Connection", client)
-
-    async def test_deprecated_path(self):
-        database = DatabaseSqlite({"file": "sqlite.db"})
-        assert database.db_file == "sqlite.db"
+@pytest.mark.asyncio
+async def test_deprecated_path():
+    database = DatabaseSqlite({"file": "sqlite.db"})
+    assert database.db_file == "sqlite.db"
