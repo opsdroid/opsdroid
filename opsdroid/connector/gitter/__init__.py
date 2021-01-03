f3c66ee308415f31fb9ec526643ee3f205e9104c
Dan Allan <daniel.b.allan@gmail.com>
2021-01-03 18:47:12 -0500

    FIX: Do not parse messages that we sent.

--- opsdroid/connector/gitter/__init__.py
+++ opsdroid/connector/gitter/__init__.py
@@ -12,6 +12,7 @@ from opsdroid.events import Message
 _LOGGER = logging.getLogger(__name__)
 GITTER_STREAM_API = "https://stream.gitter.im/v1/rooms"
 GITTER_MESSAGE_BASE_API = "https://api.gitter.im/v1/rooms"
+CURRENT_USER_API = "https://api.gitter.im/v1/user/me"
 CONFIG_SCHEMA = {Required("token"): str, Required("room-id"): str, "bot-name": str}
 
 
@@ -40,13 +41,32 @@ class ConnectorGitter(Connector):
         _LOGGER.debug(_("Connecting with Gitter stream."))
         self.session = aiohttp.ClientSession()
 
-        gitter_url = self.build_url(
+        # Gitter figures out who we are based on just our access token, but we
+        # need to additionally know our own user ID in order to know which
+        # messages comes from us.
+        current_user_url = self.build_url(
+            CURRENT_USER_API,
+            access_token=self.access_token,
+            )
+        response = await self.session.get(current_user_url, timeout=None)
+        # We cannot continue without a user ID, so raise if this failed.
+        response.raise_for_status()
+        response_json = await response.json()
+        self.bot_gitter_id = response_json["id"]
+        # Gitter figures out who we are based on
+        _LOGGER.debug(
+            _("Successfully obtained bot's gitter id, %s."),
+            self.bot_gitter_id
+        )
+
+        message_stream_url = self.build_url(
             GITTER_STREAM_API,
             self.room_id,
             "chatMessages",
             access_token=self.access_token,
         )
-        self.response = await self.session.get(gitter_url, timeout=None)
+        self.response = await self.session.get(message_stream_url, timeout=None)
+        self.response.raise_for_status()
 
     def build_url(self, base_url, *res, **params):
         """Build the url. args ex:(base_url,p1,p2=1,p2=2)."""
@@ -72,7 +92,8 @@ class ConnectorGitter(Connector):
         await asyncio.sleep(self.update_interval)
         async for data in self.response.content.iter_chunked(1024):
             message = await self.parse_message(data)
-            if message is not None:
+            # Do not parse messages that we ourselves sent.
+            if message is not None and message.user_id != self.bot_gitter_id:
                 await self.opsdroid.parse(message)
 
     async def parse_message(self, message):
@@ -85,6 +106,7 @@ class ConnectorGitter(Connector):
                 return Message(
                     text=message["text"],
                     user=message["fromUser"]["username"],
+                    user_id=message["fromUser"]["id"],
                     target=self.room_id,
                     connector=self,
                 )
