e9e727a776f6690cef424c4a920ef776487a2cc0
Dan Allan <daniel.b.allan@gmail.com>
2021-01-09 14:08:45 +0000

    Require a valid signature.

--- opsdroid/connector/sms/__init__.py
+++ opsdroid/connector/sms/__init__.py
@@ -1,8 +1,11 @@
 '''A SMS connector for opsdroid'''
+import aiohttp.web
 import asyncio
+import functools
 import logging
 
 from twilio.rest import Client
+from twilio.request_validator import RequestValidator
 from voluptuous import Required
 
 from .. import Connector, register_event
@@ -35,6 +38,8 @@ class ConnectorSMS(Connector):
         if self.config['is_trial'] and not config.get('approved_trial_numbers'):
             raise SMSConnectorException('Please set approved_trial_numbers')
 
+        self.validator = RequestValidator(config.get('auth_token'))
+
     async def connect(self):
         """Connect to Twilio and setup webhooks"""
         self.connection = await asyncio.get_running_loop().run_in_executor(
@@ -43,16 +48,29 @@ class ConnectorSMS(Connector):
             self.config["account_sid"],
             self.config["auth_token"]
         )
+        # This route must be configured manually in the Twilio web GUI.
+        # There is no API for configuring it.
+        # Watch this SO post for any developments.
+        # Last updated by Twilio as of August 2019.
+        # https://stackoverflow.com/a/57420558/1221924
         self.opsdroid.web_server.web_app.router.add_post(
             f"/connector/{self.name}", self.handle_messages
         )
 
     async def handle_messages(self, request):
-        req_data = await request.post()
+        # Validate the request using its URL, POST data,
+        # and X-TWILIO-SIGNATURE header
+        data = await request.post()
+        request_valid = self.validator.validate(
+            str(request.url),
+            data,
+            request.headers.get('X-TWILIO-SIGNATURE', ''))
+        if not request_valid:
+            raise aiohttp.web.HTTPForbidden()
         message = Message(
-            text=req_data["Body"],
-            user=req_data["From"],
-            user_id=req_data["From"],
+            text=data["Body"],
+            user=data["From"],
+            user_id=data["From"],
             connector=self,
         )
         await self.opsdroid.parse(message)
@@ -68,10 +86,15 @@ class ConnectorSMS(Connector):
             )
         else:
             await asyncio.get_running_loop().run_in_executor(
-                self.connection.messages.create,
-                from_=self.config["phone_number"],
-                to=message.user,
-                body=message.text
+                None,
+                # run_in_executor only accepts positional arguments, so
+                # partial is used here to pass in kwargs.
+                functools.partial(
+                    self.connection.messages.create,
+                    from_=self.config["phone_number"],
+                    to=message.user,
+                    body=message.text
+                )
             )
 
     async def listen(self):
