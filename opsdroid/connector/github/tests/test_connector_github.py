53a5c9dc781903ac7cc7cdd1696914cf0aeecf52
Stuart Mumford <stuart@cadair.com>
2021-01-16 15:52:50 +0000

    Fix the github tests

--- opsdroid/connector/github/tests/test_connector_github.py
+++ opsdroid/connector/github/tests/test_connector_github.py
@@ -42,49 +42,40 @@ def test_missing_token(caplog):
     assert "Missing auth token!" in caplog.text
 
 
+@pytest.mark.add_response(
+    "/user", "GET", get_response_path("github_user.json"), status=200
+)
 @pytest.mark.asyncio
 async def test_connect(connector, mock_api):
-    mock_api.add_response(
-        "/user", "GET", get_response_path("github_user.json"), status=200
-    )
+    await connector.connect()
 
-    async def test():
-        await connector.connect()
+    assert mock_api.called("/user")
+    assert mock_api.call_count("/user") == 1
 
-        assert mock_api.called("/user")
-        assert mock_api.call_count("/user") == 1
+    request = mock_api.get_request("/user")
+    assert "Authorization" in request.headers
+    assert "abc123" in request.headers["Authorization"]
 
-        request = mock_api.get_request("/user")
-        assert "Authorization" in request.headers
-        assert "abc123" in request.headers["Authorization"]
-
-        # Populated by the call to /user
-        assert connector.github_username == "opsdroid"
-
-    await mock_api.run_test(test)
+    # Populated by the call to /user
+    assert connector.github_username == "opsdroid"
 
 
+@pytest.mark.add_response(
+    "/user", "GET", get_response_path("github_user_bad_credentials.json"), status=401
+)
 @pytest.mark.asyncio
 async def test_connect_failure(connector, mock_api, caplog):
-    mock_api.add_response(
-        "/user",
-        "GET",
-        get_response_path("github_user_bad_credentials.json"),
-        status=401,
-    )
-
-    async def test():
-        await connector.connect()
-        assert "Bad credentials" in caplog.text
-
-    await mock_api.run_test(test)
+    await connector.connect()
+    assert "Bad credentials" in caplog.text
 
 
+@pytest.mark.add_response("/", "GET")
 @pytest.mark.asyncio
 async def test_disconnect(connector):
     assert await connector.disconnect() is None
 
 
+@pytest.mark.add_response("/", "GET")
 @pytest.mark.asyncio
 async def test_listen(connector):
     """Test the listen method.
@@ -97,76 +88,69 @@ async def test_listen(connector):
     assert await connector.listen() is None
 
 
+@pytest.mark.add_response(
+    "/repos/opsdroid/opsdroid/issues/1/comments", "POST", None, status=201
+)
 @pytest.mark.asyncio
 async def test_send(opsdroid, connector, mock_api):
     org, repo, issue = "opsdroid", "opsdroid", "1"
-    api_url = f"/repos/{org}/{repo}/issues/{issue}/comments"
-    mock_api.add_response(api_url, "POST", None, status=201)
-
-    async def test():
-        await opsdroid.send(
-            Message(
-                text="test",
-                user="jacobtomlinson",
-                target=f"{org}/{repo}#{issue}",
-                connector=connector,
-            )
+    await opsdroid.send(
+        Message(
+            text="test",
+            user="jacobtomlinson",
+            target=f"{org}/{repo}#{issue}",
+            connector=connector,
         )
-        assert mock_api.called(api_url)
+    )
+    assert mock_api.called("/repos/opsdroid/opsdroid/issues/1/comments")
 
-    await mock_api.run_test(test)
 
-
+@pytest.mark.add_response(
+    "/repos/opsdroid/opsdroid/issues/1/comments",
+    "POST",
+    get_response_path("github_send_failure.json"),
+    status=400,
+)
 @pytest.mark.asyncio
 async def test_send_failure(opsdroid, connector, mock_api, caplog):
     org, repo, issue = "opsdroid", "opsdroid", "1"
-    api_url = f"/repos/{org}/{repo}/issues/{issue}/comments"
-    mock_api.add_response(
-        api_url, "POST", get_response_path("github_send_failure.json"), status=400
-    )
-
-    async def test():
-        await opsdroid.send(
-            Message(
-                text="test",
-                user="jacobtomlinson",
-                target=f"{org}/{repo}#{issue}",
-                connector=connector,
-            )
+    await opsdroid.send(
+        Message(
+            text="test",
+            user="jacobtomlinson",
+            target=f"{org}/{repo}#{issue}",
+            connector=connector,
         )
-        assert mock_api.called(api_url)
-        assert "some error" in caplog.text
+    )
+    assert mock_api.called("/repos/opsdroid/opsdroid/issues/1/comments")
+    assert "some error" in caplog.text
 
-    await mock_api.run_test(test)
 
-
+@pytest.mark.add_response(
+    "/repos/opsdroid/opsdroid/issues/1/comments", "POST", status=201
+)
 @pytest.mark.asyncio
 async def test_do_not_send_to_self(opsdroid, connector, mock_api):
     org, repo, issue = "opsdroid", "opsdroid", "1"
-    api_url = f"/repos/{org}/{repo}/issues/{issue}/comments"
-    mock_api.add_response(api_url, "POST", None, status=201)
     connector.github_username = "opsdroid-bot"
 
-    async def test():
-        await opsdroid.send(
-            Message(
-                text="test",
-                user="opsdroid-bot",
-                target=f"{org}/{repo}#{issue}",
-                connector=connector,
-            )
+    await opsdroid.send(
+        Message(
+            text="test",
+            user="opsdroid-bot",
+            target=f"{org}/{repo}#{issue}",
+            connector=connector,
         )
-        assert not mock_api.called(api_url)
+    )
+    assert not mock_api.called("/repos/opsdroid/opsdroid/issues/1/comments")
 
-    await mock_api.run_test(test)
 
-
+@pytest.mark.add_response(
+    "/user", "GET", get_response_path("github_user.json"), status=200
+)
 @pytest.mark.asyncio
 async def test_receive_comment(opsdroid, connector, mock_api):
     """Test a comment create event creates a message and parses it."""
-    mock_api.add_response(
-        "/user", "GET", get_response_path("github_user.json"), status=200
-    )
 
     @match_event(Message)
     async def test_skill(opsdroid, config, event):
@@ -185,15 +169,15 @@ async def test_receive_comment(opsdroid, connector, mock_api):
         )
         assert resp.status == 201
 
-    await mock_api.run_test(run_unit_test, opsdroid, test)
+    await run_unit_test(opsdroid, test)
 
 
+@pytest.mark.add_response(
+    "/user", "GET", get_response_path("github_user.json"), status=200
+)
 @pytest.mark.asyncio
 async def test_receive_pr(opsdroid, connector, mock_api):
     """Test a PR create event creates a message and parses it."""
-    mock_api.add_response(
-        "/user", "GET", get_response_path("github_user.json"), status=200
-    )
 
     @match_event(Message)
     async def test_skill(opsdroid, config, event):
@@ -212,15 +196,15 @@ async def test_receive_pr(opsdroid, connector, mock_api):
         )
         assert resp.status == 201
 
-    await mock_api.run_test(run_unit_test, opsdroid, test)
+    await run_unit_test(opsdroid, test)
 
 
+@pytest.mark.add_response(
+    "/user", "GET", get_response_path("github_user.json"), status=200
+)
 @pytest.mark.asyncio
 async def test_receive_issue(opsdroid, connector, mock_api):
     """Test a PR create event creates a message and parses it."""
-    mock_api.add_response(
-        "/user", "GET", get_response_path("github_user.json"), status=200
-    )
 
     @match_event(Message)
     async def test_skill(opsdroid, config, event):
@@ -239,15 +223,15 @@ async def test_receive_issue(opsdroid, connector, mock_api):
         )
         assert resp.status == 201
 
-    await mock_api.run_test(run_unit_test, opsdroid, test)
+    await run_unit_test(opsdroid, test)
 
 
+@pytest.mark.add_response(
+    "/user", "GET", get_response_path("github_user.json"), status=200
+)
 @pytest.mark.asyncio
 async def test_receive_label(opsdroid, connector, mock_api):
     """Test a PR create event creates a message and parses it."""
-    mock_api.add_response(
-        "/user", "GET", get_response_path("github_user.json"), status=200
-    )
 
     test_skill = match_event(Message)(CoroutineMock())
     opsdroid.register_skill(test_skill, config={"name": "test"})
@@ -261,16 +245,16 @@ async def test_receive_label(opsdroid, connector, mock_api):
         )
         assert resp.status == 200
 
-    await mock_api.run_test(run_unit_test, opsdroid, test)
+    await run_unit_test(opsdroid, test)
     assert not test_skill.called
 
 
+@pytest.mark.add_response(
+    "/user", "GET", get_response_path("github_user.json"), status=200
+)
 @pytest.mark.asyncio
 async def test_receive_status(opsdroid, connector, mock_api):
     """Test a PR create event creates a message and parses it."""
-    mock_api.add_response(
-        "/user", "GET", get_response_path("github_user.json"), status=200
-    )
 
     test_skill = match_event(Message)(CoroutineMock())
     opsdroid.register_skill(test_skill, config={"name": "test"})
@@ -284,5 +268,5 @@ async def test_receive_status(opsdroid, connector, mock_api):
         )
         assert resp.status == 201
 
-    await mock_api.run_test(run_unit_test, opsdroid, test)
+    await run_unit_test(opsdroid, test)
     assert not test_skill.called
