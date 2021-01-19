4e60a974b7c3e49dc4cac8558645553ff97ee0ad
Stuart Mumford <stuart@cadair.com>
2021-01-19 09:57:40 +0000

    Initial hack on sync factory

--- opsdroid/connector/matrix/tests/conftest.py
+++ opsdroid/connector/matrix/tests/conftest.py
@@ -53,12 +53,47 @@ async def connector(opsdroid, request):
 
 
 ################################################################################
-# Response Fixtures
+# Sync Factory
 ################################################################################
 
 
 def get_matrix_response(name):
     return Path(__file__).parent / "responses" / f"{name}.json"
+
+
+def empty_sync(room_id):
+    with open(get_matrix_response("empty_sync_response")) as fobj:
+        empty_sync = json.load(fobj.read())
+
+    room_block = empty_sync["rooms"]["join"].pop("ROOMID")
+    empty_sync["rooms"]["join"][room_id] = room_block
+
+
+def event_factory(event_type, content, sender):
+    return {
+        "content": content,
+        "event_id": "$1234567890987654321234567890",
+        "origin_server_ts": 9876543210,
+        "sender": sender,
+        "type": event_type,
+        "unsigned": {"age": 100},
+    }
+
+
+def message_factory(body, msgtype, sender, **extra_content):
+    content = {"msgtype": msgtype, "body": body, **extra_content}
+    return event_factory("m.room.message", content, sender)
+
+
+def sync_response(events, room_id="!12345:localhost"):
+    sync = empty_sync(room_id)
+    sync["rooms"]["join"][room_id]["timeline"]["events"] = events
+    return sync
+
+
+################################################################################
+# Response Fixtures
+################################################################################
 
 
 @pytest.fixture
