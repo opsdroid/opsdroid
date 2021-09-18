dea6dabfc1c305608d581b688fad3d41fddb1e2a
Ajit D'Sa <adsa@planview.com>
2021-09-18 00:39:41 -0500

    Completed code for postgres and wrote some tests (broken at the moment)
    - Wrote a contextmanager for writing to different table from config.
    - Split out decorators into 2 separate functional decorators.
    - Split out queries for better testability.
    - Made naming conventions match the mongo conventions that were merged in.
    - Wrote first set of tests. Still need to write a few more.
    - Current tests are broken due to inability to mock in an await.

--- opsdroid/database/postgresql/__init__.py
+++ opsdroid/database/postgresql/__init__.py
@@ -4,48 +4,83 @@ import logging
 import json
 import asyncpg
 
+from contextlib import asynccontextmanager
 from opsdroid.database import Database
 from opsdroid.helper import JSONEncoder, JSONDecoder
+from voluptuous import Any, Required
 
 _LOGGER = logging.getLogger(__name__)
-CONFIG_SCHEMA = {"host": str, "password": str, "port": int, "database": str}
+CONFIG_SCHEMA = {
+    "host": str,
+    "port": Any(int, str),
+    "database": str,
+    "user": str,
+    Required("password"): str,
+    "table": str,
+}
 
-def check_table(func):
+
+def create_table_if_not_exists(func):
     """Decorator to check if the table specified exists and has correct format.
     Creates table if it does not exist"""
-    async def wrapper(*args, **kwargs):
-        # args[0].connection will get DatabasePostgres.connection
-        connection = args[0].connection
-        if 'table_name' in kwargs and kwargs['table_name']:
-            table_name = kwargs['table_name']
-        else:
-            table_name = args[0].table_name
 
-        if ' ' in table_name:
-            _LOGGER.warning('table_name contains a space character. Suggest changing "' + table_name + '" to "' + table_name.strip(' ') + '"')
+    async def wrapper(*args, **kwargs):
+        # args[0].connection will get DatabasePostgresql.connection
+        connection = args[0].connection
+        table = args[0].table
+
+        if " " in table:
+            _LOGGER.warning(
+                'table contains a space character. Suggest changing "'
+                + table
+                + '" to "'
+                + table.strip(" ")
+                + '"'
+            )
 
         async with connection.transaction():
             # Create table if it does not exist
             await connection.execute(
-                'CREATE TABLE IF NOT EXISTS "{}" ( key text PRIMARY KEY, data JSONb)'.format(table_name)
+                'CREATE TABLE IF NOT EXISTS "{}" ( key text PRIMARY KEY, data JSONb)'.format(table)
             )
             await connection.execute(
-                'CREATE INDEX IF NOT EXISTS idxgin ON "{}" USING gin (data);'.format(table_name)
+                'CREATE INDEX IF NOT EXISTS idxgin ON "{}" USING gin (data);'.format(table)
             )
-
-            # Check Table's data structure is correct
-            data_structure = await connection.fetch(
-                'SELECT column_name,data_type FROM information_schema.columns WHERE table_name = \'{}\''.format(table_name)
-            )
-            valid = len(data_structure) == 2
-            valid &= data_structure[0]['column_name'] == 'key' and data_structure[0]['data_type'] == 'text'
-            valid &= data_structure[1]['column_name'] == 'data' and data_structure[1]['data_type'] == 'jsonb'
-            if not valid:
-                _LOGGER.error('PostgresSQL table %s has incorrect data structure', table_name)
         return await func(*args, **kwargs)
+
     return wrapper
 
-class DatabasePostgres(Database):
+def check_table_format(func):
+
+    async def wrapper(*args, **kwargs):
+        # args[0].connection will get DatabasePostgresql.connection
+        connection = args[0].connection
+        table = args[0].table
+        
+        async with connection.transaction():
+            # Check Table's data structure is correct
+            data_structure = await connection.fetch(
+                'SELECT column_name,data_type FROM information_schema.columns WHERE table_name = \'{}\''.format(table)
+            )
+            valid = len(data_structure) == 2
+            valid &= (
+                data_structure[0]["column_name"] == "key"
+                and data_structure[0]["data_type"] == "text"
+            )
+            valid &= (
+                data_structure[1]["column_name"] == "data"
+                and data_structure[1]["data_type"] == "jsonb"
+            )
+            if not valid:
+                _LOGGER.error(
+                    "PostgresSQL table %s has incorrect data structure", table
+                )
+        return await func(*args, **kwargs)
+
+    return wrapper
+
+
+class DatabasePostgresql(Database):
     """A module for opsdroid to allow memory to persist in a PostgreSQL database."""
 
     def __init__(self, config, opsdroid=None):
@@ -62,32 +97,30 @@ class DatabasePostgres(Database):
         """
         super().__init__(config, opsdroid=opsdroid)
         _LOGGER.debug("Loaded PostgreSQL database connector.")
-        self.name = "postgres"
+        self.name = "postgresql"
         self.config = config
         self.connection = None
-        self.user = self.config.get("user", "opsdroid")
-        self.password = self.config.get("password")
-        self.database = self.config.get("database", "opsdroid")
-        self.table_name = self.config.get("default_table_name", "opsdroid_default")
-        self.host = self.config.get("host", "localhost")
-        self.port = self.config.get("port", 5432)
-
-        _LOGGER.debug("Loaded postgres database connector")
+        self.table = self.config.get("table", "opsdroid")
 
     async def connect(self):
+        host = self.config.get("host", "localhost")
+        port = self.config.get("port", 5432)
+        database = self.config.get("database", "opsdroid")
+        user = self.config.get("user", "opsdroid")
+        pwd = self.config.get("password")
         self.connection = await asyncpg.connect(
-            user=self.user,
-            password=self.password,
-            database=self.database,
-            host=self.host,
-            port=self.port
+            user=user, password=pwd, database=database, host=host, port=port
         )
+        _LOGGER.info("Connected to PostgreSQL.")
 
     async def disconnect(self):
-        await self.connection.close()
+        if self.connection:
+            await self.connection.close()
+            _LOGGER.info("Disconnected from PostgreSQL.")
 
-    @check_table
-    async def put(self, key, data, table_name=""):
+    @create_table_if_not_exists
+    @check_table_format
+    async def put(self, key, data):
         """Insert or replace an object into the database for a given key.
 
         Args:
@@ -95,70 +128,87 @@ class DatabasePostgres(Database):
             data (object): the data to be inserted or replaced
 
         """
-        if table_name:
-            self.table_name = table_name
 
-        _LOGGER.debug("Putting %s into PostgreSQL table %s", key, self.table_name)
+        _LOGGER.debug("Putting %s into PostgreSQL table %s", key, self.table)
         if isinstance(data, str):
             data = {"value": data}
         json_data = json.dumps(data, cls=JSONEncoder)
+        await self.put_query(key, json_data)
 
+    async def put_query(self, key, json_data):
         async with self.connection.transaction():
-            key_already_exists = await self.get(key, table_name=self.table_name)
+            key_already_exists = await self.get(key)
             if key_already_exists:
                 await self.connection.execute(
-                    'UPDATE "{}" SET data = $2 WHERE key = $1'.format(self.table_name),
-                    key, json_data
+                    'UPDATE "{}" SET data = $2 WHERE key = $1'.format(self.table),
+                    key,
+                    json_data,
                 )
             else:
                 await self.connection.execute(
-                    'INSERT INTO "{}" VALUES ($1, $2)'.format(self.table_name),
-                    key, json_data
+                    'INSERT INTO "{}" VALUES ($1, $2)'.format(self.table),
+                    key,
+                    json_data,
                 )
 
-    @check_table
-    async def get(self, key, table_name=""):
+    @check_table_format
+    async def get(self, key):
         """Get a document from the database (key).
 
         Args:
             key (str): the key is the database name.
 
         """
-        if table_name:
-            self.table_name = table_name
 
-        _LOGGER.debug("Getting %s from PostgreSQL table %s", key, self.table_name)
+        _LOGGER.debug("Getting %s from PostgreSQL table %s", key, self.table)
 
-        values = await self.connection.fetch(
-            'SELECT data FROM "{}" WHERE key = $1'.format(self.table_name),
-            key,
-        )
-
-        if (len(values) == 1) and values[0]['data']:
-            data = json.loads(values[0]['data'], object_hook=JSONDecoder())
+        values = await self.get_query(key)
+        
+        if (len(values) == 1) and values[0]["data"]:
+            data = json.loads(values[0]["data"], object_hook=JSONDecoder())
             if data.keys() == {"value"}:
                 data = data["value"]
             return data
         elif len(values) > 1:
-            _LOGGER.error(str(len(values)) + ' entries with same key name in PostgresSQL table %s', self.table_name)
+            _LOGGER.error(
+                str(len(values))
+                + " entries with same key name in PostgresSQL table %s",
+                self.table,
+            )
         else:
             return None
 
-    @check_table
-    async def delete(self, key, table_name=""):
+    async def get_query(self, key):
+        return await self.connection.fetch(
+            'SELECT data FROM "{}" WHERE key = $1'.format(self.table),
+            key,
+        )
+
+    @check_table_format
+    async def delete(self, key):
         """Delete a document from the database (key).
 
         Args:
             key (str): the key is the database name.
 
         """
-        if table_name:
-            self.table_name = table_name
 
-        _LOGGER.debug("Deleting %s from PostgreSQL table %s.", key, self.table_name)
+        _LOGGER.debug("Deleting %s from PostgreSQL table %s.", key, self.table)
+        await self.delete_query(key)
 
+    async def delete_query(self, key):
         async with self.connection.transaction():
             await self.connection.execute(
-                'DELETE FROM "{}" WHERE key = $1'.format(self.table_name),
-                key
+                'DELETE FROM "{}" WHERE key = $1'.format(self.table), key
             )
+
+    @asynccontextmanager
+    async def memory_in_table(self, table):
+        """Use the specified collection rather than the default."""
+        db_copy = DatabasePostgresql(self.config, self.opsdroid)
+        try:
+            await db_copy.connect()
+            db_copy.table = table
+            yield db_copy
+        finally:
+            await db_copy.disconnect()
---
+++ opsdroid/database/postgresql/tests/test_postgresql.py
@@ -0,0 +1,53 @@
+import pytest
+
+from opsdroid.database.postgresql import DatabasePostgresql
+
+
+@pytest.fixture()
+def database(config):
+    return DatabasePostgresql(config)
+
+
+@pytest.mark.parametrize("config", [{"database": "test_db"}])
+def test_init(database):
+    """Test that the database is initialised properly."""
+    assert database.name == "postgresql"
+    assert database.config["database"] == "test_db"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("config", [{}])
+async def test_connect(mocker, database):
+    """Test that the mongo database has implemented connect function properly"""
+    try:
+        mocker.patch("asyncpg.connect", return_value=mocker.AsyncMock())
+        await database.connect()
+    except NotImplementedError:
+        raise Exception
+    else:
+        pass
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("config", [{"table": "test_table"}])
+async def test_put(mocker, database):
+    mocker.patch.object(database, "put_query", return_value={})
+    await database.put("test_key", {"value": "test_value"})
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("config", [{"table": "test_table"}])
+@pytest.mark.parametrize("return_value", [
+        [],
+        [{"data": "{\"value\": \"test_value\"}"}],
+        [{"data": "{\"value\": \"test_value1\"}"}, {"data": "{\"value\": \"test_value2\"}"}]
+    ])
+async def test_get(mocker, database, return_value):
+    mocker.patch.object(database, "get_query", return_value=return_value)
+    await database.get("test_key")
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("config", [{"table": "test_table"}])
+async def test_delete(mocker, database):
+    mocker.patch.object(database, "delete_query", return_value={})
+    await database.delete("test_key")
