9c3a8ea4ef2eb70a974e797402682d65a34466e0
MrNaif2018 <39452697+MrNaif2018@users.noreply.github.com>
2021-10-05 13:39:44 +0300

    Use aioredis 2.0 for redis database (#1835)

--- opsdroid/database/redis/tests/test_redis.py
+++ opsdroid/database/redis/tests/test_redis.py
@@ -31,7 +31,9 @@ def test_init(caplog):
 async def test_connect(mocker, caplog):
     caplog.set_level(logging.DEBUG)
     database = RedisDatabase({})
-    mocked_connection = mocker.patch("aioredis.create_pool")
+    mocked_connection = mocker.patch(
+        "aioredis.Redis.ping", return_value=return_async_value(True)
+    )
 
     await database.connect()
 
@@ -43,7 +45,7 @@ async def test_connect(mocker, caplog):
 async def test_connect_failure(mocker, caplog):
     caplog.set_level(logging.DEBUG)
     database = RedisDatabase({})
-    mocked_connection = mocker.patch("aioredis.create_pool", side_effect=OSError)
+    mocked_connection = mocker.patch("aioredis.Redis.ping", side_effect=OSError)
 
     with suppress(OSError):
         await database.connect()
@@ -57,13 +59,15 @@ async def test_get(mocker, caplog):
     caplog.set_level(logging.DEBUG)
     database = RedisDatabase({})
     database.client = mocker.Mock()
-    attrs = {"execute.return_value": return_async_value('{"data_key":"data_value"}')}
+    attrs = {
+        "execute_command.return_value": return_async_value('{"data_key":"data_value"}')
+    }
     database.client.configure_mock(**attrs)
 
     result = await database.get("key")
 
     assert result == dict(data_key="data_value")
-    database.client.execute.assert_called_with("GET", "key")
+    database.client.execute_command.assert_called_with("GET", "key")
     assert "Getting" in caplog.text
 
 
@@ -72,7 +76,7 @@ async def test_get_return_none(mocker, caplog):
     caplog.set_level(logging.DEBUG)
     database = RedisDatabase({})
     database.client = mocker.Mock()
-    attrs = {"execute.return_value": return_async_value(None)}
+    attrs = {"execute_command.return_value": return_async_value(None)}
     database.client.configure_mock(**attrs)
 
     result = await database.get("key")
@@ -86,12 +90,12 @@ async def test_put(mocker, caplog):
     caplog.set_level(logging.DEBUG)
     database = RedisDatabase({})
     database.client = mocker.Mock()
-    attrs = {"execute.return_value": return_async_value("None")}
+    attrs = {"execute_command.return_value": return_async_value("None")}
     database.client.configure_mock(**attrs)
 
     await database.put("key", dict(data_key="data_value"))
 
-    database.client.execute.assert_called_with(
+    database.client.execute_command.assert_called_with(
         "SET", "key", json.dumps(dict(data_key="data_value"), cls=JSONEncoder)
     )
     assert "Putting" in caplog.text
@@ -102,12 +106,12 @@ async def test_delete(mocker, caplog):
     caplog.set_level(logging.DEBUG)
     database = RedisDatabase({})
     database.client = mocker.Mock()
-    attrs = {"execute.return_value": return_async_value("None")}
+    attrs = {"execute_command.return_value": return_async_value("None")}
     database.client.configure_mock(**attrs)
 
     await database.delete("key")
 
-    database.client.execute.assert_called_with("DEL", "key")
+    database.client.execute_command.assert_called_with("DEL", "key")
     assert "Deleting" in caplog.text
 
 
