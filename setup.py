1c19959783b6066c9379307ec69c9a649072991f
Mustafa Ali <62649206+mustafamusse@users.noreply.github.com>
2022-03-05 18:03:25 +0100

    Breaking the program to test

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@
 from setuptools import setup
 from setuptools.command.build_py import build_py
 from setuptools.command.sdist import sdist
-from setuptools.command.develop import develop
-from setuptools.config import read_configuration
+from setuimport read_configuration
 from itertools import chain
 import versioneer
 
