5bdf76e66a10b881d08291e9ed4b3ee730b6b147
l21scist <luca.scistri@imt-atlantique.net>
2022-11-29 13:59:34 +0100

    Copied Facebook connector tests and added the necessary tokens for the bots

--- opsdroid/connector/discord/__init__.py
+++ opsdroid/connector/discord/__init__.py
@@ -14,23 +14,32 @@ CONFIG_SCHEMA = {
     "bot-name": str,
 }
 
-class ConnectorDiscord(Connector):
 
+class ConnectorDiscord(Connector):
     def __init__(self, config, opsdroid=None):
         """Connector Setup."""
         super().__init__(config, opsdroid=opsdroid)
         _LOGGER.debug("Starting Discord Connector.")
         self.name = config.get("name", "discord")
         self.bot_name = config.get("bot-name", "opsdroid")
-        self.token = config["token"]
+        self.token = config.get("token")
         self.client = DiscordClient(self.handle_message)
         self.bot_id = None
-    
-    async def handle_message(self, text,user,user_id,target,msg):
-        event = Message(text=text,user=user, user_id=user_id, target=target, connector=self,raw_event=msg)
-        _LOGGER.info("-----------------------------------------------"+user+" said "+text)
+
+    async def handle_message(self, text, user, user_id, target, msg):
+        event = Message(
+            text=text,
+            user=user,
+            user_id=user_id,
+            target=target,
+            connector=self,
+            raw_event=msg,
+        )
+        _LOGGER.info(
+            "-----------------------------------------------" + user + " said " + text
+        )
         await self.opsdroid.parse(event)
-    
+
     async def connect(self):
         await self.client.start(self.token)
 
@@ -43,16 +52,12 @@ class ConnectorDiscord(Connector):
         """Respond with a message."""
         _LOGGER.debug(_("Responding to Discord."))
         await message.target.send(message.text)
-    
+
     async def disconnect(self):
         _LOGGER.debug(_("disconnecting"))
         pr("disconnecting")
-        #_LOGGER.debug(_("disconnecting"))
+        # _LOGGER.debug(_("disconnecting"))
         self.client.close()
         pr("disconnecting done")
         self.client.join()
         # for now, the thread is terminated
-
-def pr(mes):
-    with open("D:\Documents\Ecole\\02IMTAtlantique\IDL\debug.txt", "a") as f:
-        f.write(mes + "\n")
