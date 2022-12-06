f2ae562732f362a5a5a19c031e1157b6f104e02c
Severin <s21cheva@imt-atlantique.net>
2022-12-06 12:55:01 +0100

    add file and image handler from Arnaud

--- opsdroid/connector/discord/__init__.py
+++ opsdroid/connector/discord/__init__.py
@@ -1,11 +1,14 @@
 """A connector for Discord."""
 import logging
-from .client import DiscordClient
+import io
+import aiohttp
+
+from .client import DiscordClient, discordMessage, discordFile
 
 from voluptuous import Required
 
 from opsdroid.connector import Connector, register_event
-from opsdroid.events import Message
+from opsdroid.events import Message,File,Image
 
 _LOGGER = logging.getLogger(__name__)
 _DISCORD_SEND_URL = "https://discord.com/api"
@@ -29,7 +32,7 @@ class ConnectorDiscord(Connector):
         self.name = config.get("name", "discord")
         self.bot_name = config.get("bot-name", "opsdroid")
         self.token = config.get("token")
-        self.client = DiscordClient(self.handle_message, self.rename)
+        self.client = DiscordClient(self.rename, self.handle_message, self.handle_image ,self.handle_file)
         self.bot_id = None
     
     async def rename(self):
@@ -39,20 +42,40 @@ class ConnectorDiscord(Connector):
         if self.bot_name != self.client.user.name :
             await self.client.user.edit(username=self.bot_name)
 
-    async def handle_message(self, text, user, user_id, target, msg):
+    async def handle_message(self, message:discordMessage):
         """
-        Handle messages received from discord gateway. All the arguments are provided by the 
+        Handle messages received from discord gateway. For now, it can be text message, file or an image
         """
-        event = Message(
-            text=text,
-            user=user,
-            user_id=user_id,
-            target=target,
-            connector=self,
-            raw_event=msg,
-        )
-        _LOGGER.info(user + " said " + text)
+        event = Message(text=message.content, user=message.author.name, user_id=message.author.id, target=message.channel, connector=self, raw_event=message)
+        _LOGGER.info(message.author.name + " said " + message.content)
         await self.opsdroid.parse(event)
+    
+    async def handle_image(self, message:discordMessage, img_nb:int):
+        image = message.attachments[img_nb]
+        event = Image(url=image.url, name=image.filename ,user=message.author.name, user_id=message.author.id, target=message.channel, connector=self,raw_event=message, event_id=img_nb)
+        _LOGGER.info(message.author.name+" sent the image at "+image.url)
+        await self.opsdroid.parse(event)
+
+    async def handle_file(self, message:discordMessage, file_nb:int):
+        file = message.attachments[file_nb]
+        event = File(url=file.url, name=file.filename, user=message.author.name, user_id=message.author.id, target=message.channel, connector=self,raw_event=message, event_id=file_nb)
+        _LOGGER.info(message.author.name+" sent the file at "+file.url)
+        await self.opsdroid.parse(event)
+    
+    @register_event(Image)
+    async def send_image(self, image_event:Image):
+        """Send image to Discord."""
+        _LOGGER.debug(_("Sending image to Discord."))
+        data = io.BytesIO(await image_event.get_file_bytes())
+        await image_event.target.send(file=discordFile(data, image_event.name))
+
+    @register_event(File)
+    async def send_file(self, file_event:File):
+        """Send file to Discord."""
+        _LOGGER.debug(_("Sending file to Discord."))
+        data = io.BytesIO(await file_event.get_file_bytes())
+        await file_event.target.send(file=discordFile(fp=data, filename=file_event.name))
+
 
     async def connect(self):
         """Connect to the discord gateway to get messages sent to the bot"""
--- opsdroid/connector/discord/client.py
+++ opsdroid/connector/discord/client.py
@@ -1,6 +1,8 @@
 import logging
 _LOGGER = logging.getLogger(__name__)
 import discord
+from discord import Message as discordMessage
+from discord import File as discordFile
 
 
 class DiscordClient(discord.Client):
@@ -8,11 +10,13 @@ class DiscordClient(discord.Client):
     subclass of discord.Client
     It is used according to the discord library
     """
-    def __init__(self, handle_message_func, rename_func):
+    def __init__(self, rename_func, handle_message_func, handle_image_func, handle_file_func):
         intents = discord.Intents(messages=True, message_content=True)
         super().__init__(intents=intents)
-        self.handle_message_func = handle_message_func
         self.rename_func = rename_func
+        self.handle_message_func = handle_message_func
+        self.handle_image_func = handle_image_func
+        self.handle_file_func = handle_file_func
         discord.utils.setup_logging( 
                 level=_LOGGER.level, # same level of login as opsdroid
                 root=False,
@@ -23,12 +27,21 @@ class DiscordClient(discord.Client):
         _LOGGER.debug('Logged in as '+str(self.user.name)+" "+str(self.user.id))
         await self.rename_func() # we rename according to the name given in bot-name
 
-    async def on_message(self, message):
+    async def on_message(self, message:discordMessage):
         """When the bot receive a message"""
         # we do not want the bot to reply to itself
         if message.author.id == self.user.id:
             return
         else :
             # we use the function given in parameter of the class
-            await self.handle_message_func(message.content, message.author.name, message.author.id, message.channel, message)
+            await self.handle_message_func(message)
+             # we test if a file is attached
+            if (len(message.attachments) > 0):
+                for i in range(len(message.attachments)):
+                    file = message.attachments[i]
+                    # then we test if one of the attached files is an image
+                    if file.filename.endswith(('.jpg','.jpeg','.png')):
+                        await self.handle_image_func(message, i)
+                    else :
+                        await self.handle_file_func(message, i)
             
