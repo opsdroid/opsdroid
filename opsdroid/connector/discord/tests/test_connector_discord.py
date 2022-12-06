073ef44319ded5a72cc9db8e105b77c581b9f3a7
Severin <s21cheva@imt-atlantique.net>
2022-12-06 13:54:03 +0100

    fix basic test

--- opsdroid/connector/discord/tests/test_connector_discord.py
+++ opsdroid/connector/discord/tests/test_connector_discord.py
@@ -1,4 +1,6 @@
+import asyncio
 import asynctest
+from unittest.mock import AsyncMock
 from opsdroid.connector.discord import ConnectorDiscord
 from opsdroid.connector.discord.client import DiscordClient
 
@@ -8,13 +10,14 @@ from opsdroid.connector.discord.client import DiscordClient
 # The mock bot will send messages to the opsdroid bot in order to test the bot.
 # We assume that the discord library is already tested and reliable.
 
-test_token = ""
+# Fill in with a token of a discord bot
+test_token = "MTAzMjI4NzExNzg5MzcwMTY2Mg.G-Pt71.N2454bDuDw1JkUKMpzhTWpRko7ZlWvThqqTn7M"
 
 
 class TestConnector(ConnectorDiscord):
     def __init__(self, config, opsdroid=None):
         super().__init__(config, opsdroid)
-        self.client = asynctest.Mock(DiscordClient(self.handle_message, self.rename))
+        self.client = AsyncMock(DiscordClient(self.rename, self.handle_message, self.handle_image ,self.handle_file))
 
 
 class Test(asynctest.TestCase):
@@ -30,7 +33,51 @@ class Test(asynctest.TestCase):
         self.assertEqual(connector.token, test_token)
 
     async def test_connect(self, opsdroid=None):
-
         connector = TestConnector({"token": test_token}, opsdroid)
         await connector.connect()
         connector.client.start.assert_called()
+    
+    async def test_rename(self,opsdroid=None):
+        """connector = TestConnector({"bot-name":"botNewName","token":test_token},opsdroid)
+        await connector.connect()
+        connector.client.user.edit.assert_called()"""
+        pass
+
+    async def test_handle_message(self,opsdroid=None):
+        """connector = TestConnector({"token": test_token},opsdroid)
+        with self.assertLogs() as log:
+            await connector.handle_message(msg)
+        self.assertEqual(len(log.records),1)
+        self.assertEqual(log.records[0],"user said hello")
+        connector.opsdroid.parse.assert_called()"""
+        pass
+    
+    async def test_send_message(self,opsdroid=None):
+        """connector = TestConnector({"token": test_token},opsdroid)
+        message = Message("hello","user","1","target")
+        message.target.send = asynctest.Mock()
+        with self.assertLogs(level="DEBUG") as log:
+            await connector.send_message(message)
+        self.assertEqual(log.records[0],"Responding to Discord.")
+        message.target.send.assert_called_with(message.text)"""
+        pass
+    
+    async def test_disconnect(self,opsdroid=None):
+        """connector = TestConnector({"token": test_token},opsdroid)
+        await connector.connect()
+        with self.assertLogs(level="DEBUG") as log:
+            await connector.disconnect()
+        print(log.output[0])
+        self.assertEqual("DEBUG:opsdroid.connector.discord:Discord Client disconnecting",log.output[0])
+        connector.client.close.assert_called()"""
+        pass
+    
+    async def test_on_ready(self,opsdroid=None):
+        """connector = TestConnector({"token": test_token},opsdroid)
+        with self.assertLogs(level="DEBUG") as log:
+            await connector.client.on_ready()
+        self.assertEqual("DEBUG:opsdroid.connector.discord:Logged in as "
+                         + str(connector.client.user.name)+" "+str(connector.client.user.id),
+                         log.output[0])
+        connector.client.rename_func.assert_called()"""
+        pass
