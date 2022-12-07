e9d699e84597815231cae18c86a1c045d8673a4b
Rodrigo Albuquerque <ralbuquerque@apple.com>
2022-12-07 15:17:19 +0000

    Opsdroid can now respond within a Thread in Interactive Block Actions

--- opsdroid/connector/slack/events.py
+++ opsdroid/connector/slack/events.py
@@ -75,15 +75,18 @@ class InteractiveAction(events.Event):
             if "response_url" in self.payload:
                 async with aiohttp.ClientSession() as session:
                     headers = {"Content-Type": "application/json"}
+                    response_data = {
+                                            "text": response_event,
+                                            "replace_original": False,
+                                            "delete_original": False,
+                                            "response_type": "in_channel",
+                                        }
+                    if 'thread_ts' in self.payload['message']:
+                        response_data["thread_ts"] = self.payload["message"]["thread_ts"]
                     response = await session.post(
                         self.payload["response_url"],
                         data=json.dumps(
-                            {
-                                "text": response_event,
-                                "replace_original": False,
-                                "delete_original": False,
-                                "response_type": "in_channel",
-                            }
+                            response_data
                         ),
                         headers=headers,
                         ssl=self.ssl_context,
