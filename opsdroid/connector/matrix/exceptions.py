1766ca945b8ca32a50f4dc28cecf8e2a48f60319
Stuart Mumford <stuart@cadair.com>
2023-03-04 11:49:56 +0000

    Error better

--- opsdroid/connector/matrix/exceptions.py
+++ opsdroid/connector/matrix/exceptions.py
@@ -3,3 +3,9 @@ class MatrixException(Exception):
 
     def __init__(self, nio_error):
         self.nio_error = nio_error
+
+    def __repr__(self):
+        return super().__repr__ + "\n" + str(self.nio_error)
+
+    def __str__(self):
+        return super().__str__ + "\n" + str(self.nio_error)
