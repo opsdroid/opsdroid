b28f0843b8f03f2eb72ce4598107ea8f3202c391
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2024-04-25 10:13:24 +0100

    Update versioneer (#2024)

--- opsdroid/__init__.py
+++ opsdroid/__init__.py
@@ -1,5 +1,5 @@
 """Init file for opsdroid."""
-from ._version import get_versions
 
-__version__ = get_versions()["version"]
-del get_versions
+from . import _version
+
+__version__ = _version.get_versions()["version"]
