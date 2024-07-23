4821bea65571e8b460630be10ca5a56e2b90cb1c
Jonatan Pretelt <jonatan.pretelt@gmail.com>
2024-07-23 19:23:56 -0400

    Fix linting  issues

--- opsdroid/cli/utils.py
+++ opsdroid/cli/utils.py
@@ -44,7 +44,9 @@ def edit_config(ctx, path):
 
     """
     file = path or DEFAULT_CONFIG_PATH
-    editor = os.environ.get("EDITOR", "notepad.exe" if platform.system() == "Windows" else "vi")
+    editor = os.environ.get(
+        "EDITOR", "notepad.exe" if platform.system() == "Windows" else "vi"
+    )
 
     if editor == "vi":
         click.echo(
