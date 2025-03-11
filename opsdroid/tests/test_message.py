16b561260ea81c0d95a8b6ccd0ede92bba1b3a95
Jacob Tomlinson <jacob@tomlinson.email>
2025-03-11 10:32:23 +0000

    Remove unused import

--- opsdroid/tests/test_message.py
+++ opsdroid/tests/test_message.py
@@ -1,13 +1,11 @@
-import asynctest
+import pytest
+
+import warnings
 
 from opsdroid.core import OpsDroid
 from opsdroid.message import Message
 from opsdroid.connector import Connector
 from opsdroid.cli.start import configure_lang
-
-import pytest
-import warnings
-
 
 
 @pytest.mark.anyio
@@ -15,11 +13,11 @@ async def setup():
     configure_lang({})
 
 
-@pytest.mark.anyio 
+@pytest.mark.anyio
 async def test_message(raw_message):
     with OpsDroid() as opsdroid:
         mock_connector = Connector({}, opsdroid=opsdroid)
-        
+
         message = Message(
             text="Hello world",
             user="user",
@@ -33,7 +31,7 @@ async def test_message(raw_message):
         assert message.target == "default"
         assert message.raw_event["timestamp"] == "01/01/2000 19:23:00"
         assert message.raw_event["messageId"] == "101"
-        
+
         with pytest.raises(TypeError):
             await message.respond("Goodbye world")
 
@@ -43,21 +41,32 @@ def test_depreacted_properties():
 
     message.target = "spam"
     with pytest.warns(DeprecationWarning):
-        warnings.warn("opsdroid.message.Message is deprecated. Please use opsdroid.events.Message instead", DeprecationWarning)
+        warnings.warn(
+            "opsdroid.message.Message is deprecated. Please use opsdroid.events.Message instead",
+            DeprecationWarning,
+        )
         assert message.room == "spam"
 
     with pytest.warns(DeprecationWarning):
-        warnings.warn("Message.room is deprecated. Use Message.target instead", DeprecationWarning)
+        warnings.warn(
+            "Message.room is deprecated. Use Message.target instead", DeprecationWarning
+        )
         message.room = "eggs"
 
     assert message.room == "eggs"
 
     message.raw_event = "spam"
     with pytest.warns(DeprecationWarning):
-        warnings.warn("Message.raw_event  deprecated. Please use opsdroid.event.Message instead", DeprecationWarning)
+        warnings.warn(
+            "Message.raw_event  deprecated. Please use opsdroid.event.Message instead",
+            DeprecationWarning,
+        )
         assert message.raw_message == "spam"
 
     with pytest.warns(DeprecationWarning):
-        warnings.warn("Message.raw_message is deprecated. Use Message.raw_event instead.", DeprecationWarning)
+        warnings.warn(
+            "Message.raw_message is deprecated. Use Message.raw_event instead.",
+            DeprecationWarning,
+        )
         message.raw_message = "eggs"
     assert message.raw_event == "eggs"
